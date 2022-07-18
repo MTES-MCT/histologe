f17502ff84c5ed68cd8c05b6742de20c72eea4b0
Emilien Schneider <emilien.schneider@beta.gouv.fr>
2022-07-18 16:47:40 +0200

    feat: index - replacement of error message by modal when postal code is not managed

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -30,6 +30,8 @@ class FrontController extends AbstractController
         $stats['pec'] = $stats['total'] !== 0 ? floor(($affectationRepository->createQueryBuilder('a')->select('COUNT(DISTINCT a.signalement)')->join('a.signalement', 'signalement', 'WITH', 'signalement.statut != 7 AND YEAR(signalement.createdAt) = ' . $year)->getQuery()->getSingleScalarResult() / $stats['total']) * 100) : 0;
         $stats['res'] = $stats['total'] !== 0 ? floor(($affectationRepository->createQueryBuilder('a')->select('COUNT(DISTINCT a.signalement)')->where('a.statut = 1')->join('a.signalement', 'signalement', 'WITH', 'signalement.statut != 7 AND YEAR(signalement.createdAt) = ' . $year)->getQuery()->getSingleScalarResult() / $stats['total']) * 100) : 0;
         
+        $display_modal = '';
+
         $form = $this->createForm(PostalCodeSearchType::class, []);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -42,13 +44,14 @@ class FrontController extends AbstractController
                 return $this->redirect($redirectUrl);
             }
 
-            $this->addFlash('error', "Le territoire ".$inputPostalCode." n'est pas encore disponible sur Histologe. Merci de réessayer ultérieurement.");
+            $display_modal = $inputPostalCode;
         }
         
         return $this->render('front/index.html.twig', [
-            'title' => $title,
-            'form_postalcode' => $form->createView(),
-            'stats' => $stats
+            'title'             => $title,
+            'form_postalcode'   => $form->createView(),
+            'stats'             => $stats,
+            'display_modal'     => $display_modal,
         ]);
     }
 
@@ -68,12 +71,17 @@ class FrontController extends AbstractController
         $form = $this->createForm(ContactType::class, []);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $notificationService->send(NotificationService::TYPE_CONTACT_FORM, ['notifications@histologe.fr', $configurationService->get()->getEmailReponse()], [
-                'nom' => $form->get('nom')->getData(),
-                'mail' => $form->get('email')->getData(),
-                'reply' => $form->get('email')->getData(),
-                'message' => nl2br($form->get('message')->getData()),
-            ]);
+            $notificationService->send(
+                NotificationService::TYPE_CONTACT_FORM,
+                ['notifications@histologe.fr', $configurationService->get()->getEmailReponse()],
+                [
+                    'nom' => $form->get('nom')->getData(),
+                    'mail' => $form->get('email')->getData(),
+                    'reply' => $form->get('email')->getData(),
+                    'message' => nl2br($form->get('message')->getData()),
+                ],
+                null
+            );
             $this->addFlash('success', 'Votre message à bien été envoyé !');
             return $this->redirectToRoute('front_contact');
         }
