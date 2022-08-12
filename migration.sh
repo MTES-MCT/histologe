d2379592bd7a6a19bb3dbc4b8f94d0b26d57f88a
Saidi <saidi.ahamada@gmail.com>
2022-08-12 11:38:04 +0200

    [infra]: add comments for skipping the first migration

--- migration.sh
+++ migration.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+# Skip the first migration
 php bin/console d:m:version 'DoctrineMigrations\Version20220811093406' --add --no-interaction 2> /dev/null
+# Execute the other migrations
 php bin/console d:m:m --no-interaction
