105d66c0dd0f157460b4a2c17a601fd834e86ef8
Saidi <saidi.ahamada@gmail.com>
2022-08-12 15:50:30 +0200

    handle scalingo deployment

--- migration.sh
+++ migration.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Skip the first migration
-php bin/console d:m:version 'DoctrineMigrations\Version20220811093406' --add --no-interaction 2> /dev/null
-# Execute the other migrations
-php bin/console d:m:m --no-interaction
+
+if [[ -z "${SKIP_FIRST_MIGRATION}" ]]; # Execute the skip migration in main application
+then
+    echo "Skip the first migration"
+    php bin/console d:m:version 'DoctrineMigrations\Version20220811093406' --add --no-interaction
+fi
+php bin/console d:m:m --no-interaction # Execute migrations
