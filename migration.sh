100b72535ecd2db6a586ccccf75dda0c3f3ac3b5
Saidi <saidi.ahamada@gmail.com>
2022-08-16 09:59:37 +0200

    remove migration.sh

--- migration.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -z "${SKIP_FIRST_MIGRATION}" ]]; # Execute the skip migration in main application
-then
-    echo "Skip the first migration"
-    php bin/console d:m:version 'DoctrineMigrations\Version20220811093406' --add --no-interaction
-fi
-php bin/console d:m:m --no-interaction # Execute migrations
