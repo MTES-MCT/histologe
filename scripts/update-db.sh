84e93260df4534eaa83f82c800b2e63bf01d697b
Saidi <saidi.ahamada@gmail.com>
2022-08-24 16:43:49 +0200

    debug fuxtures scalingo deployment

--- scripts/update-db.sh
+++ scripts/update-db.sh
@@ -6,6 +6,7 @@ if [[ -z "${COMPOSER_DEV}" ]];
 then
     echo "No fixtures to load!"
 else
+    echo "Fixtures to load!"
     # composer dump-env dev
     # php bin/console doctrine:fixtures:load --no-interaction
 fi
