53a3f4e2bdc8aa2985bc32fc9bca1a45e3c2e727
Saidi <saidi.ahamada@gmail.com>
2022-08-30 10:06:48 +0200

    add log for deployment

--- scripts/update-db.sh
+++ scripts/update-db.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Create temp folder..."
+if [ ! -d var/temp ];
+then
+  mkdir -p var/temp
+  echo "Folder temp created"
+else
+  echo "Folder temp already created"
+fi
+
 php bin/console doctrine:migrations:migrate --no-interaction
 
 if [[ -z "${COMPOSER_DEV}" ]];
@@ -9,5 +18,3 @@ else
     echo "Fixtures to load!"
     composer dump-env dev && php bin/console -e dev doctrine:fixtures:load --no-interaction
 fi
-
-mkdir var/temp 2>/dev/null
