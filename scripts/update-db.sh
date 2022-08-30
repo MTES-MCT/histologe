ab54cfd272b814fbb286627d840e206523941bf6
Saidi <saidi.ahamada@gmail.com>
2022-08-30 11:44:37 +0200

    update tmp directory

--- scripts/update-db.sh
+++ scripts/update-db.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-echo "Create temp folder..."
-if [ ! -d var/temp ];
-then
-  mkdir -p var/temp
-  echo "Folder temp created"
-else
-  echo "Folder temp already created"
-fi
-
+echo "Executing migration..."
 php bin/console doctrine:migrations:migrate --no-interaction
 
 if [[ -z "${COMPOSER_DEV}" ]];
 then
-    echo "No fixtures to load!"
+    echo "No data fixtures to load!"
 else
-    echo "Fixtures to load!"
+    echo "Load data fixtures..."
     composer dump-env dev && php bin/console -e dev doctrine:fixtures:load --no-interaction
 fi
