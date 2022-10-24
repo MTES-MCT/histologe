c075e5a437fb2ca35c9d906a98a3f84bfc0fd5bc
Emilien Schneider <emilien.schneider@beta.gouv.fr>
2022-10-24 16:59:02 +0200

    add query to init is_imported

--- migrations/Version20221024135042.php
+++ migrations/Version20221024135042.php
@@ -17,6 +17,7 @@ final class Version20221024135042 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE signalement ADD is_imported BOOLEAN DEFAULT NULL');
+        $this->addSql('UPDATE signalement SET is_imported = 1 WHERE YEAR(created_at) < 2022 AND territory_id != 64 AND territory_id != 59');
     }
 
     public function down(Schema $schema): void
