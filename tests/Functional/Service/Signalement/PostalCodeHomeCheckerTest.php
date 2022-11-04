802e96fd9f22f8b87430dcdf30b34ca71fbbfaba
Emilien Schneider <emilien.schneider@beta.gouv.fr>
2022-11-04 16:21:06 +0100

    add test for insee code checker

--- tests/Functional/Service/Signalement/PostalCodeHomeCheckerTest.php
+++ tests/Functional/Service/Signalement/PostalCodeHomeCheckerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional\Service\Signalement;
 
+use App\Repository\TerritoryRepository;
 use App\Service\Signalement\PostalCodeHomeChecker;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -34,5 +35,28 @@ class PostalCodeHomeCheckerTest extends KernelTestCase
     public function testInactiveTerritory(): void
     {
         $this->assertFalse($this->postalCodeHomeChecker->isActive('75001'));
+    }
+
+    public function testTerritoryWithNoAuthorizedInseeCodes(): void
+    {
+        $territoryRepository = static::getContainer()->get(TerritoryRepository::class);
+        $territory = $territoryRepository->findOneBy(['id' => 1]);
+        $this->assertTrue($this->postalCodeHomeChecker->isAuthorizedInseeCode($territory, '01000'));
+    }
+
+    public function testTerritoryWithAuthorizedInseeCodesAndInseeCodeValid(): void
+    {
+        $territoryRepository = static::getContainer()->get(TerritoryRepository::class);
+        $territory = $territoryRepository->findOneBy(['id' => 70]);
+        $territory->setAuthorizedCodesInsee(['69091']);
+        $this->assertTrue($this->postalCodeHomeChecker->isAuthorizedInseeCode($territory, '69091'));
+    }
+
+    public function testTerritoryWithAuthorizedInseeCodesAndInseeCodeNotValid(): void
+    {
+        $territoryRepository = static::getContainer()->get(TerritoryRepository::class);
+        $territory = $territoryRepository->findOneBy(['id' => 70]);
+        $territory->setAuthorizedCodesInsee(['69091']);
+        $this->assertFalse($this->postalCodeHomeChecker->isAuthorizedInseeCode($territory, '69092'));
     }
 }
