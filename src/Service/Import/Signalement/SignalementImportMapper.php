db779ac5da0bb0709b6932b3205ffa917e3e5382
Saidi <saidi.ahamada@gmail.com>
2023-01-10 16:52:56 +0100

    fix: remove status autre by defaut make motif_cloture upper

--- src/Service/Import/Signalement/SignalementImportMapper.php
+++ src/Service/Import/Signalement/SignalementImportMapper.php
@@ -199,7 +199,7 @@ class SignalementImportMapper
                         $fieldValue = $this->transformToSignalementStatus($fieldValue);
                         break;
                     case 'motifCloture':
-                        $fieldValue = \array_key_exists($fieldValue, MotifCloture::LABEL) ? $fieldValue : 'AUTRE';
+                        $fieldValue = \array_key_exists(mb_strtoupper($fieldValue), MotifCloture::LABEL) ? $fieldValue : null;
                         break;
                     case self::SITUATION_SECURITE_OCCUPANT:
                     case self::SITUATION_ETAT_PROPRETE_LOGEMENT:
