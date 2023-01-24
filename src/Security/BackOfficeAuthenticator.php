4a3c8be7d0cb41cdd3308ad49b4c359299a565ac
emilschn <emilien.schneider@beta.gouv.fr>
2023-01-24 18:05:56 +0100

    fix login to avoid archived users

--- src/Security/BackOfficeAuthenticator.php
+++ src/Security/BackOfficeAuthenticator.php
@@ -2,11 +2,14 @@
 
 namespace App\Security;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -22,10 +25,10 @@ class BackOfficeAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    private UrlGeneratorInterface $urlGenerator;
-
-    public function __construct(UrlGeneratorInterface $urlGenerator)
-    {
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private UserRepository $userRepository,
+    ) {
         $this->urlGenerator = $urlGenerator;
     }
 
@@ -41,6 +44,11 @@ class BackOfficeAuthenticator extends AbstractLoginFormAuthenticator
     public function authenticate(Request $request): Passport
     {
         $email = $request->request->get('email', '');
+
+        $accountArchived = $this->userRepository->findOneBy(['email' => $email, 'statut' => User::STATUS_ARCHIVE]);
+        if ($accountArchived) {
+            throw new CustomUserMessageAuthenticationException('Ce compte a été supprimé.');
+        }
 
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
