3570f0c5f1b3671f7728326142d14a59a1278180
emilschn <emilien.schneider@beta.gouv.fr>
2023-01-26 10:22:41 +0100

    change method to check if user is not archived when login

--- src/Security/BackOfficeAuthenticator.php
+++ src/Security/BackOfficeAuthenticator.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -45,15 +44,12 @@ class BackOfficeAuthenticator extends AbstractLoginFormAuthenticator
     {
         $email = $request->request->get('email', '');
 
-        $accountArchived = $this->userRepository->findOneBy(['email' => $email, 'statut' => User::STATUS_ARCHIVE]);
-        if ($accountArchived) {
-            throw new CustomUserMessageAuthenticationException('Ce compte a été supprimé.');
-        }
-
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
         return new Passport(
-            new UserBadge($email),
+            new UserBadge($email, function (string $email) {
+                return $this->userRepository->findOneBy(['email' => $email, 'statut' => [User::STATUS_ACTIVE, User::STATUS_INACTIVE]]);
+            }),
             new PasswordCredentials($request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
