076844eb921053d20c8d986e0eaa3f1ccb505724
Saidi <saidi.ahamada@gmail.com>
2023-02-06 18:08:56 +0100

    fix: remove comment

--- migrations/Version20230202092736.php
+++ migrations/Version20230202092736.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230202092736 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,14 +16,12 @@ final class Version20230202092736 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE INDEX idx_signalement_statut ON signalement (statut)');
         $this->addSql('CREATE INDEX idx_signalement_created_at ON signalement (created_at)');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP INDEX idx_signalement_statut ON signalement');
         $this->addSql('DROP INDEX idx_signalement_created_at ON signalement');
     }
