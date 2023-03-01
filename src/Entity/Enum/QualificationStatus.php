d527e719b64c0a3f68596c5384baa4e815a7fc35
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-03-01 11:59:37 +0100

    932 : édition de NDE et droit d'accès à cette partie

--- src/Entity/Enum/QualificationStatus.php
+++ src/Entity/Enum/QualificationStatus.php
@@ -4,6 +4,7 @@ namespace App\Entity\Enum;
 
 enum QualificationStatus: string
 {
+    case ARCHIVED = 'archived';
     case NDE_AVEREE = 'Non décence énergétique avérée';
     case NDE_OK = 'Décence énergétique OK';
     case NDE_CHECK = 'Non décence énergétique à vérifier';
