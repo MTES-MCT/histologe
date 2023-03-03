52ad588691a813f4486440555f06043b9da46277
emilschn <emilien.schneider@beta.gouv.fr>
2023-03-03 15:49:27 +0100

    save nde for front signalement, and change display

--- src/Service/Signalement/SignalementQualificationService.php
+++ src/Service/Signalement/SignalementQualificationService.php
@@ -14,6 +14,14 @@ class SignalementQualificationService
 
     public function updateNDEStatus(): ?QualificationStatus
     {
+        if (null == $this->signalementQualification->getDernierBailAt()) {
+            return QualificationStatus::NDE_CHECK;
+        }
+
+        if ($this->signalementQualification->getDernierBailAt()->format('Y') >= '2023' && '0' === $this->signalementQualification->getDetails()['DPE']) {
+            return QualificationStatus::NDE_AVEREE;
+        }
+
         if ($this->signalementQualification->getDernierBailAt()->format('Y') >= '2023' && $this->signalementQualification->getDetails()['consommation_energie'] > 450) {
             return QualificationStatus::NDE_AVEREE;
         }
