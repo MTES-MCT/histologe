c5070d321202272f36b683a4e1e5263654ab34e0
Saidi <saidi.ahamada@gmail.com>
2023-03-06 12:37:41 +0100

    cd: run composer dump-autoload

--- scripts/postdeploy.sh
+++ scripts/postdeploy.sh
@@ -2,6 +2,8 @@
 
 echo "Executing migration..."
 php bin/console doctrine:migrations:migrate --no-interaction
+composer dump-autoload --no-dev --classmap-authoritative
+php bin/console c:c
 
 if [[ -z "${COMPOSER_DEV}" ]];
 then
