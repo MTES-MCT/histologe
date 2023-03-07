fa74b34322ce226861deea7a3511c9b3ab40fb3a
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-03-07 22:44:46 +0100

    932 : serilize DTO

--- src/Controller/Back/BackSignalementQualificationController.php
+++ src/Controller/Back/BackSignalementQualificationController.php
@@ -2,34 +2,33 @@
 
 namespace App\Controller\Back;
 
-use App\Dto\SignalementQualificationNDE;
+use App\Dto\Request\Signalement\QualificationNDERequest;
 use App\Entity\Signalement;
 use App\Entity\SignalementQualification;
 use App\Manager\SignalementManager;
-use App\Service\Signalement\SignalementQualificationService;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/bo/signalements')]
 class BackSignalementQualificationController extends AbstractController
 {
-    #[Route('/{uuid}/qualification/{signalement_qualification}/editer', name: 'back_signalement_qualification_editer')]
+    #[Route('/{uuid}/qualification/{signalement_qualification}/editer', name: 'back_signalement_qualification_editer', methods: 'POST')]
     public function editQualification(
         Request $request,
         Signalement $signalement,
         SignalementQualification $signalementQualification,
-        EntityManagerInterface $entityManager,
         SignalementManager $signalementManager,
-        SignalementQualificationService $signalementQualificationService,
-        SignalementQualificationNDE $signalementQualificationNDE
+        SerializerInterface $serializer
     ): RedirectResponse|JsonResponse {
         $this->denyAccessUnlessGranted('SIGN_EDIT_NDE', $signalement);
-        if ($this->isCsrfTokenValid('signalement_edit_nde_'.$signalement->getId(), $request->get('_token'))) {
-            $signalementManager->updateFromSignalementQualification($signalement, $signalementQualification, $signalementQualificationNDE);
+        $decodedRequest = json_decode($request->getContent());
+        if ($this->isCsrfTokenValid('signalement_edit_nde_'.$signalement->getId(), $decodedRequest->_token)) {
+            $qualificationNDERequest = $serializer->deserialize($request->getContent(), QualificationNDERequest::class, 'json');
+            $signalementManager->updateFromSignalementQualification($signalementQualification, $qualificationNDERequest);
         } else {
             $this->addFlash('error', "Une erreur est survenu lors de l'édition");
         }
