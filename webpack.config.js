3074069756c380240a06451903f5edf66fc73613
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-03-10 17:12:24 +0100

    992 : copy files in public

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,13 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
+
+
+    .copyFiles({
+        from: './assets/files',
+        to: 'files/[path][name].[ext]'
+    })
+
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
