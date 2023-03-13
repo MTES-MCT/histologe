ee56162ea37b15a4744cac48eb006c3d2f9f7968
Saidi <saidi.ahamada@gmail.com>
2023-03-13 10:11:16 +0100

    feat: update signalement list by calling new managaer

--- src/Controller/Back/BackController.php
+++ src/Controller/Back/BackController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
 
 #[Route('/bo')]
 class BackController extends AbstractController
@@ -20,7 +19,6 @@ class BackController extends AbstractController
         Request $request,
         SearchFilterService $searchFilterService,
         SearchFilterOptionDataProvider $searchFilterOptionDataProvider,
-        CacheInterface $cache,
         SignalementManager $signalementManager,
     ): Response {
         $title = 'Administration - Tableau de bord';
