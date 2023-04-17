95a7a24d88b87336a3ab808713757e4ee62e3e39
Saidi <saidi.ahamada@gmail.com>
2023-04-17 12:06:21 +0200

    add service #1119

---
+++ src/Service/Esabora/EsaboraSISHService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Service\Esabora;
+
+class EsaboraSISHService
+{
+    public function pushAdresse(): void
+    {
+        // SISH_ADRESSE
+    }
+
+    public function pushDossier(): void
+    {
+        // SISH_DOSSIER
+    }
+
+    public function pushPersonne(): void
+    {
+        // SISH_ADRESSE
+    }
+
+    public function getStateDossier(): void
+    {
+        // WS_ETAT_DOSSIER_SAS
+    }
+}
