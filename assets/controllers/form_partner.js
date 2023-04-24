632db3516f89db88ff5abca26af00a7cea2c1203
emilschn <emilien.schneider@beta.gouv.fr>
2023-04-24 15:19:48 +0200

    test factorize js code to avoid sonarcloud alert

--- assets/controllers/form_partner.js
+++ assets/controllers/form_partner.js
@@ -1,11 +1,35 @@
+function histoUpdateSubmitButton(elementName, elementLabel) {
+  document.querySelector(elementName).innerHTML = elementLabel
+  document.querySelector(elementName).disabled = true
+}
+function histoUpdateFieldsVisibility(showZonePDL, showEsaboraTitle, showEsaboraDiv) {
+  if (showZonePDL) {
+    document.querySelector('#partner_create_zone_pdl').classList.remove('fr-hidden')
+  } else {
+    document.querySelector('#partner_create_zone_pdl').classList.add('fr-hidden')
+  }
+  if (showEsaboraTitle) {
+    document.querySelector('#partner_create_esabora_title').classList.remove('fr-hidden')
+  } else {
+    document.querySelector('#partner_create_esabora_title').classList.add('fr-hidden')
+  }
+  if (showEsaboraDiv) {
+    document.querySelector('#partner_create_esabora_div').classList.remove('fr-hidden')
+  } else {
+    document.querySelector('#partner_create_esabora_div').classList.add('fr-hidden')
+  }
+}
+function histoUpdateValueFromData(elementName, elementData) {
+  document.querySelector(elementName).value = target.getAttribute(elementData)
+}
+
 document.querySelectorAll('.btn-transfer-partner-user').forEach(swbtn => {
   swbtn.addEventListener('click', evt => {
     const target = evt.target
     document.querySelector('#fr-modal-user-transfer_username').innerHTML = target.getAttribute('data-username')
-    document.querySelector('#fr-modal-user-transfer_userid').value = target.getAttribute('data-userid')
+    histoUpdateValueFromData('#fr-modal-user-transfer_userid', 'data-userid')
     document.querySelector('#user_transfer_form').addEventListener('submit', (e) => {
-      document.querySelector('#user_transfer_form_submit').innerHTML = 'Transfert en cours...'
-      document.querySelector('#user_transfer_form_submit').disabled = true
+      histoUpdateSubmitButton('#user_transfer_form_submit', 'Transfert en cours...')
     })
   })
 })
@@ -18,10 +42,9 @@ document.querySelectorAll('.btn-delete-partner-user').forEach(swbtn => {
     document.querySelectorAll('.fr-modal-user-delete_useremail').forEach(userItem => {
       userItem.innerHTML = target.getAttribute('data-useremail')
     })
-    document.querySelector('#fr-modal-user-delete_userid').value = target.getAttribute('data-userid')
+    histoUpdateValueFromData('#fr-modal-user-delete_userid', 'data-userid')
     document.querySelector('#user_delete_form').addEventListener('submit', (e) => {
-      document.querySelector('#user_delete_form_submit').innerHTML = 'Suppression en cours...'
-      document.querySelector('#user_delete_form_submit').disabled = true
+      histoUpdateSubmitButton('#user_delete_form_submit', 'Suppression en cours...')
     })
   })
 })
@@ -32,10 +55,9 @@ document.querySelectorAll('.btn-delete-partner').forEach(swbtn => {
     document.querySelectorAll('.fr-modal-partner-delete_name').forEach(userItem => {
       userItem.innerHTML = target.getAttribute('data-partnername')
     })
-    document.querySelector('#fr-modal-partner-delete_partnerid').value = target.getAttribute('data-partnerid')
+    histoUpdateValueFromData('#fr-modal-partner-delete_partnerid', 'data-partnerid')
     document.querySelector('#partner_delete_form').addEventListener('submit', (e) => {
-      document.querySelector('#partner_delete_form_submit').innerHTML = 'Suppression en cours...'
-      document.querySelector('#partner_delete_form_submit').disabled = true
+      histoUpdateSubmitButton('#partner_delete_form_submit', 'Suppression en cours...')
     })
   })
 })
@@ -46,10 +68,10 @@ document.querySelectorAll('.btn-edit-partner-user').forEach(swbtn => {
     document.querySelectorAll('.fr-modal-user-edit_useremail').forEach(userItem => {
       userItem.innerHTML = target.getAttribute('data-useremail')
     })
-    document.querySelector('#user_edit_userid').value = target.getAttribute('data-userid')
-    document.querySelector('#user_edit_email').value = target.getAttribute('data-useremail')
-    document.querySelector('#user_edit_nom').value = target.getAttribute('data-usernom')
-    document.querySelector('#user_edit_prenom').value = target.getAttribute('data-userprenom')
+    histoUpdateValueFromData('#user_edit_userid', 'data-userid')
+    histoUpdateValueFromData('#user_edit_email', 'data-useremail')
+    histoUpdateValueFromData('#user_edit_nom', 'data-usernom')
+    histoUpdateValueFromData('#user_edit_prenom', 'data-userprenom')
     const isMailingActive = target.getAttribute('data-userismailingactive')
     if ( '1' === isMailingActive ) {
       document.querySelector('#user_edit_is_mailing_active-1').checked = true
@@ -72,28 +94,21 @@ document.querySelectorAll('.btn-edit-partner-user').forEach(swbtn => {
     }
 
     document.querySelector('#user_edit_form').addEventListener('submit', (e) => {
-      document.querySelector('#user_edit_form_submit').innerHTML = 'Edition en cours...'
-      document.querySelector('#user_edit_form_submit').disabled = true
+      histoUpdateSubmitButton('#user_edit_form_submit', 'Edition en cours...')
     })
   })
 })
 
 if (document.querySelector('#partner_type')) {
   document.querySelector('#partner_type').addEventListener('change', (event) => {
-      const partner_type = document.getElementById("partner_type");
-      partner_type.value = partner_type.value.toUpperCase();
-      if (partner_type.value === 'COMMUNE_SCHS') {
-          document.querySelector('#partner_create_zone_pdl').classList.remove('fr-hidden')
-          document.querySelector('#partner_create_esabora_title').classList.remove('fr-hidden')
-          document.querySelector('#partner_create_esabora_div').classList.remove('fr-hidden')
-      } else if (partner_type.value === 'EPCI') {
-          document.querySelector('#partner_create_zone_pdl').classList.remove('fr-hidden')
-          document.querySelector('#partner_create_esabora_title').classList.add('fr-hidden')
-          document.querySelector('#partner_create_esabora_div').classList.add('fr-hidden')
-      } else {
-          document.querySelector('#partner_create_zone_pdl').classList.add('fr-hidden')
-          document.querySelector('#partner_create_esabora_title').classList.add('fr-hidden')
-          document.querySelector('#partner_create_esabora_div').classList.add('fr-hidden')
-      }
+    const partner_type = document.getElementById("partner_type");
+    partner_type.value = partner_type.value.toUpperCase();
+    if (partner_type.value === 'COMMUNE_SCHS') {
+      histoUpdateFieldsVisibility(true, true, true)
+    } else if (partner_type.value === 'EPCI') {
+      histoUpdateFieldsVisibility(true, false, false)
+    } else {
+      histoUpdateFieldsVisibility(false, false, false)
+    }
   });
 }
