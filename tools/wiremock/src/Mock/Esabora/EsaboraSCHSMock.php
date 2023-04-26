49a403df441d6f044129c88398f9c9af5a77c240
Saidi <saidi.ahamada@gmail.com>
2023-04-26 07:39:33 +0200

    update mock #1119

--- tools/wiremock/src/Mock/Esabora/EsaboraSCHSMock.php
+++ tools/wiremock/src/Mock/Esabora/EsaboraSCHSMock.php
@@ -21,76 +21,61 @@ class EsaboraSCHSMock extends AbstractEsaboraMock
             self::RESOURCES_DIR,
         );
 
-        self::createPushDossierMock(
+        self::createCustomStateDossierMock(
             $wiremock,
-            'doSearch',
-            'WS_ETAT_SAS',
-            'ws_etat_sas.json',
-            self::BASE_PATH,
-            self::RESOURCES_DIR,
-            self::REQUEST_SEARCH_NAME
+            '00000000-0000-0000-2022-000000000008',
+            'etat_a_traiter.json'
         );
+
+        self::createCustomStateDossierMock(
+            $wiremock,
+            '00000000-0000-0000-2022-000000000001',
+            'etat_importe.json'
+        );
+
+        self::createCustomStateDossierMock(
+            $wiremock,
+            '00000000-0000-0000-2022-000000000002',
+            'etat_non_importe.json'
+        );
+
+        self::createCustomStateDossierMock(
+            $wiremock,
+            '00000000-0000-0000-2022-000000000010',
+            'etat_termine.json'
+        );
+
+        $uuids = [
+            '00000000-0000-0000-2022-000000000001',
+            '00000000-0000-0000-2022-000000000002',
+            '00000000-0000-0000-2022-000000000010',
+            '00000000-0000-0000-2022-000000000008',
+        ];
 
         self::createStateDossierMock(
             $wiremock,
             'doSearch',
             WireMock::matchingJsonPath(
                 '$.criterionList[0].criterionValueList[0]',
-                WireMock::equalTo('00000000-0000-0000-2022-000000000008')
-            ),
-            'ws_etat_dossier_sas/etat_a_traiter.json',
-            self::BASE_PATH,
-            self::RESOURCES_DIR,
-            self::REQUEST_SEARCH_NAME
-        );
-
-        self::createStateDossierMock(
-            $wiremock,
-            'doSearch',
-            WireMock::matchingJsonPath(
-                '$.criterionList[0].criterionValueList[0]',
-                WireMock::equalTo('00000000-0000-0000-2022-000000000001')
-            ),
-            'ws_etat_dossier_sas/etat_importe.json',
-            self::BASE_PATH,
-            self::RESOURCES_DIR,
-            self::REQUEST_SEARCH_NAME
-        );
-
-        self::createStateDossierMock(
-            $wiremock,
-            'doSearch',
-            WireMock::matchingJsonPath(
-                '$.criterionList[0].criterionValueList[0]',
-                WireMock::equalTo('00000000-0000-0000-2022-000000000002')
-            ),
-            'ws_etat_dossier_sas/etat_non_importe.json',
-            self::BASE_PATH,
-            self::RESOURCES_DIR,
-            self::REQUEST_SEARCH_NAME
-        );
-
-        self::createStateDossierMock(
-            $wiremock,
-            'doSearch',
-            WireMock::matchingJsonPath(
-                '$.criterionList[0].criterionValueList[0]',
-                WireMock::equalTo('00000000-0000-0000-2022-000000000010')
-            ),
-            'ws_etat_dossier_sas/etat_termine.json',
-            self::BASE_PATH,
-            self::RESOURCES_DIR,
-            self::REQUEST_SEARCH_NAME
-        );
-
-        self::createStateDossierMock(
-            $wiremock,
-            'doSearch',
-            WireMock::matchingJsonPath(
-                '$.criterionList[0].criterionValueList[0]',
-                WireMock::notMatching('00000000-0000-0000-2022-000000000001|00000000-0000-0000-2022-000000000002|00000000-0000-0000-2022-000000000010|00000000-0000-0000-2022-000000000008')
+                WireMock::notMatching(implode('|', $uuids))
             ),
             'ws_etat_dossier_sas/etat_non_trouve.json',
+            self::BASE_PATH,
+            self::RESOURCES_DIR,
+            self::REQUEST_SEARCH_NAME
+        );
+    }
+
+    private static function createCustomStateDossierMock(WireMock $wiremock, string $uuid, string $jsonFilename): void
+    {
+        self::createStateDossierMock(
+            $wiremock,
+            'doSearch',
+            WireMock::matchingJsonPath(
+                '$.criterionList[0].criterionValueList[0]',
+                WireMock::equalTo($uuid)
+            ),
+            'ws_etat_dossier_sas/'.$jsonFilename,
             self::BASE_PATH,
             self::RESOURCES_DIR,
             self::REQUEST_SEARCH_NAME
