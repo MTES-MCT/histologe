ec2b7f149a0dcb5fe6b6fb42adfff5e3cedb456e
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-05-11 13:40:46 +0200

    1205 : test image64encoder

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -5,16 +5,22 @@ namespace App\Twig;
 use App\Entity\Enum\QualificationStatus;
 use App\Service\Notification\NotificationCounter;
 use App\Service\Signalement\QualificationStatusService;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
+    public function __construct(private NormalizerInterface $normalizer)
+    {
+    }
+
     public function getFilters(): array
     {
         return [
             new TwigFilter('status_to_css', [$this, 'getCssFromStatus']),
+            new TwigFilter('image64', [$this, 'createBase64Image']),
         ];
     }
 
@@ -39,5 +45,10 @@ class AppExtension extends AbstractExtension
             new TwigFunction('can_see_nde_qualification', [QualificationStatusService::class, 'canSeenNDEQualification']),
             new TwigFunction('can_see_nde_edit_zone', [QualificationStatusService::class, 'canSeenNDEEditZone']),
         ];
+    }
+
+    public function createBase64Image(string $imagePath): string
+    {
+        return (string) $this->normalizer->normalize(new \SplFileObject($imagePath));
     }
 }
