b597062327b7e3b784d14366004798ca83808bd0
emilschn <emilien.schneider@beta.gouv.fr>
2023-05-12 11:40:34 +0200

    use data provider in NotificationControllerTest

--- tests/Functional/Controller/NotificationControllerTest.php
+++ tests/Functional/Controller/NotificationControllerTest.php
@@ -18,6 +18,84 @@ class NotificationControllerTest extends WebTestCase
         self::ensureKernelShutdown();
     }
 
+    /**
+     * @dataProvider provideAllNotificationOptions
+     */
+    public function testAllNotifications(string $route, string $tokenName, string $tokenId): void
+    {
+        $client = static::createClient();
+        /** @var UrlGeneratorInterface $generatorUrl */
+        $generatorUrl = static::getContainer()->get(UrlGeneratorInterface::class);
+
+        /** @var UserRepository $userRepository */
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy(['email' => 'admin-01@histologe.fr']);
+        $client->loginUser($user);
+        $route = $generatorUrl->generate($route, [
+            $tokenName => $this->generateCsrfToken($client, $tokenId),
+        ]);
+
+        $client->request('GET', $route);
+        $this->assertResponseRedirects('/bo/notifications');
+    }
+
+    private function provideAllNotificationOptions(): \Generator
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy(['email' => 'admin-01@histologe.fr']);
+
+        yield 'Mark as read' => [
+            'back_notifications_list_read',
+            'mark_as_read',
+            'mark_as_read_'.$user->getId(),
+        ];
+        yield 'Delete' => [
+            'back_notifications_list_delete',
+            'delete_all_notifications',
+            'delete_all_notifications_'.$user->getId(),
+        ];
+    }
+
+    /**
+     * @dataProvider provideSelectedNotificationOptions
+     */
+    public function testSelectedNotifications(string $route, string $tokenId): void
+    {
+        $client = static::createClient();
+        /** @var UrlGeneratorInterface $generatorUrl */
+        $generatorUrl = static::getContainer()->get(UrlGeneratorInterface::class);
+
+        /** @var UserRepository $userRepository */
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy(['email' => 'admin-01@histologe.fr']);
+        $client->loginUser($user);
+
+        $notificationsId = $this->getSelectedNotifications($user);
+
+        $route = $generatorUrl->generate($route, [
+            'csrf_token' => $this->generateCsrfToken($client, $tokenId),
+            'selected_notifications' => $notificationsId,
+        ]);
+
+        $client->request('GET', $route);
+        $this->assertResponseRedirects('/bo/notifications');
+    }
+
+    private function provideSelectedNotificationOptions(): \Generator
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy(['email' => 'admin-01@histologe.fr']);
+
+        yield 'Mark as read' => [
+            'back_notifications_list_read',
+            'mark_as_read_'.$user->getId(),
+        ];
+        yield 'Delete' => [
+            'back_notifications_list_delete',
+            'delete_notifications_'.$user->getId(),
+        ];
+    }
+
     private function getSelectedNotifications(User $user): string
     {
         $notificationRepository = static::getContainer()->get(NotificationRepository::class);
@@ -28,85 +106,5 @@ class NotificationControllerTest extends WebTestCase
         }
 
         return implode('', $notificationsId);
-    }
-
-    public function testMarkAsReadAllNotifications(): void
-    {
-        $client = static::createClient();
-        /** @var UrlGeneratorInterface $generatorUrl */
-        $generatorUrl = static::getContainer()->get(UrlGeneratorInterface::class);
-
-        /** @var UserRepository $userRepository */
-        $userRepository = static::getContainer()->get(UserRepository::class);
-        $user = $userRepository->findOneBy(['email' => 'admin-01@histologe.fr']);
-        $client->loginUser($user);
-        $route = $generatorUrl->generate('back_notifications_list_read', [
-            'mark_as_read' => $this->generateCsrfToken($client, 'mark_as_read_'.$user->getId()),
-        ]);
-
-        $client->request('GET', $route);
-        $this->assertResponseRedirects('/bo/notifications');
-    }
-
-    public function testMarkAsReadSelectedNotifications(): void
-    {
-        $client = static::createClient();
-        /** @var UrlGeneratorInterface $generatorUrl */
-        $generatorUrl = static::getContainer()->get(UrlGeneratorInterface::class);
-
-        /** @var UserRepository $userRepository */
-        $userRepository = static::getContainer()->get(UserRepository::class);
-        $user = $userRepository->findOneBy(['email' => 'admin-01@histologe.fr']);
-        $client->loginUser($user);
-
-        $notificationsId = $this->getSelectedNotifications($user);
-
-        $route = $generatorUrl->generate('back_notifications_list_read', [
-            'csrf_token' => $this->generateCsrfToken($client, 'mark_as_read_'.$user->getId()),
-            'selected_notifications' => $notificationsId,
-        ]);
-
-        $client->request('GET', $route);
-        $this->assertResponseRedirects('/bo/notifications');
-    }
-
-    public function testDeleteAllNotifications(): void
-    {
-        $client = static::createClient();
-        /** @var UrlGeneratorInterface $generatorUrl */
-        $generatorUrl = static::getContainer()->get(UrlGeneratorInterface::class);
-
-        /** @var UserRepository $userRepository */
-        $userRepository = static::getContainer()->get(UserRepository::class);
-        $user = $userRepository->findOneBy(['email' => 'admin-01@histologe.fr']);
-        $client->loginUser($user);
-        $route = $generatorUrl->generate('back_notifications_list_delete', [
-            'delete_all_notifications' => $this->generateCsrfToken($client, 'delete_all_notifications_'.$user->getId()),
-        ]);
-
-        $client->request('GET', $route);
-        $this->assertResponseRedirects('/bo/notifications');
-    }
-
-    public function testDeleteSelectedNotifications(): void
-    {
-        $client = static::createClient();
-        /** @var UrlGeneratorInterface $generatorUrl */
-        $generatorUrl = static::getContainer()->get(UrlGeneratorInterface::class);
-
-        /** @var UserRepository $userRepository */
-        $userRepository = static::getContainer()->get(UserRepository::class);
-        $user = $userRepository->findOneBy(['email' => 'admin-01@histologe.fr']);
-        $client->loginUser($user);
-
-        $notificationsId = $this->getSelectedNotifications($user);
-
-        $route = $generatorUrl->generate('back_notifications_list_delete', [
-            'csrf_token' => $this->generateCsrfToken($client, 'delete_all_notifications_'.$user->getId()),
-            'selected_notifications' => $notificationsId,
-        ]);
-
-        $client->request('GET', $route);
-        $this->assertResponseRedirects('/bo/notifications');
     }
 }
