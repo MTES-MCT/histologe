bddcac9697e68e30408981aff2500339a838b90e
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-05-15 15:32:15 +0200

    1205 : no file duplicated in temp

--- src/Service/Files/ImageBase64Encoder.php
+++ src/Service/Files/ImageBase64Encoder.php
@@ -14,12 +14,10 @@ class ImageBase64Encoder implements RuntimeExtensionInterface
 
     public function encode(string $filename): string
     {
-        $tmpFilepath = $this->parameterBag->get('uploads_tmp_dir').$filename;
         $bucketFilepath = $this->parameterBag->get('url_bucket').'/'.$filename;
-        file_put_contents($tmpFilepath, file_get_contents($bucketFilepath));
 
-        $type = pathinfo($tmpFilepath, \PATHINFO_EXTENSION);
-        $data = base64_encode(file_get_contents($tmpFilepath));
+        $type = pathinfo($bucketFilepath, \PATHINFO_EXTENSION);
+        $data = base64_encode(file_get_contents($bucketFilepath));
 
         return "data:image/$type;base64,$data";
     }
