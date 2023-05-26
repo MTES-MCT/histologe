4ad9fc3e76cc41df4398fca80b0ed40d355429cc
Saidi <saidi.ahamada@gmail.com>
2023-05-26 11:33:35 +0200

    get zipcode 3 digits for martinique #1248

--- src/Service/Signalement/PostalCodeHomeChecker.php
+++ src/Service/Signalement/PostalCodeHomeChecker.php
@@ -9,9 +9,10 @@ class PostalCodeHomeChecker
 {
     public const CORSE_DU_SUD_CODE_DEPARTMENT_2A = '2A';
     public const HAUTE_CORSE_CODE_DEPARTMENT_2B = '2B';
+    public const MARTNIQUE_CODE_DEPARTMENT_972 = '972';
     public const LA_REUNION_CODE_DEPARTMENT_974 = '974';
 
-    public function __construct(private TerritoryRepository $territoryRepository)
+    public function __construct(private readonly TerritoryRepository $territoryRepository)
     {
     }
 
@@ -40,6 +41,7 @@ class PostalCodeHomeChecker
         return match ($zipChunk) {
             '200', '201' => self::CORSE_DU_SUD_CODE_DEPARTMENT_2A,
             '202', '206' => self::HAUTE_CORSE_CODE_DEPARTMENT_2B,
+            '972' => self::MARTNIQUE_CODE_DEPARTMENT_972,
             '974' => self::LA_REUNION_CODE_DEPARTMENT_974,
             default => substr($postalCode, 0, 2),
         };
