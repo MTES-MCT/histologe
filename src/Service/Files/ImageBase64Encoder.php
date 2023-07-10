35c278366ed593dfd52525950671566ee04ab3fa
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-07-10 16:48:23 +0200

    1453 : check photo file exist for pdf export

--- src/Service/Files/ImageBase64Encoder.php
+++ src/Service/Files/ImageBase64Encoder.php
@@ -12,13 +12,21 @@ class ImageBase64Encoder implements RuntimeExtensionInterface
     ) {
     }
 
-    public function encode(string $filename): string
+    public function encode(?string $filename): ?string
     {
-        $bucketFilepath = $this->parameterBag->get('url_bucket').'/'.$filename;
+        if (null !== $filename) {
+            $bucketFilepath = $this->parameterBag->get('url_bucket').'/'.$filename;
 
-        $type = pathinfo($bucketFilepath, \PATHINFO_EXTENSION);
-        $data = base64_encode(file_get_contents($bucketFilepath));
+            $type = pathinfo($bucketFilepath, \PATHINFO_EXTENSION);
 
-        return "data:image/$type;base64,$data";
+            $data = @file_get_contents($bucketFilepath);
+            if (false !== $data) {
+                $data64 = base64_encode($data);
+
+                return "data:image/$type;base64,$data64";
+            }
+        }
+
+        return null;
     }
 }
