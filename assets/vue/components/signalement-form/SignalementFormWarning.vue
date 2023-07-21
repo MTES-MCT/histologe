820a49d04a1fb78f2124900cfe6d6e1aeb4ec26f
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-07-21 17:27:18 +0200

    1494 : composant SignalementFormWarning

---
+++ assets/vue/components/signalement-form/SignalementFormWarning.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="fr-alert fr-alert--warning fr-my-5w">
+        <p class="fr-alert__title" v-html="label"></p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SignalementFormWarning',
+  props: {
+    id: { type: String, default: null },
+    label: { type: String, default: null }
+  }
+})
+</script>
+
+<style>
+</style>
