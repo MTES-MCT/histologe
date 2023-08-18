1d74ad8a9939bf05b7cf5186130bc3db3d2adc89
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-08-18 15:34:50 +0200

    1504 : add progressbar and delete button to file

--- assets/vue/components/signalement-form/components/SignalementFormUpload.vue
+++ assets/vue/components/signalement-form/components/SignalementFormUpload.vue
@@ -1,22 +1,21 @@
 <template>
 <div :class="['fr-upload-group', { 'fr-upload-group--disabled': disabled }]" :id="id">
-  <label class='fr-label' :for="id">
+  <label class='fr-label' :for="id" >
     {{ label }}
     <span class="fr-hint-text">{{ description }}</span>
   </label>
     <div :class="[ customCss, 'fr-upload-wrap' ]">
       <input
-            type="file"
-            :name="id"
-            :value="internalValue"
-            :class="[ customCss, 'fr-upload' ]"
-            @input="updateValue($event)"
-            aria-describedby="text-upload-error-desc-error"
-            :disabled="disabled"
-            @change="uploadFile($event)"
-            >
-            <!-- TODO : mettre multiple ? -->
-            <!-- TODO : gérer type de fichier accept=".pdf,.doc,.docx" -->
+        type="file"
+        :name="id"
+        class="fr-upload"
+        aria-describedby="text-upload-error-desc-error"
+        :disabled="disabled"
+        @change="uploadFile($event)"
+        >
+        <p v-if="formStore.data[id] !== undefined">{{ uploadedFileTitle }}</p>
+        <!-- TODO : mettre multiple ? -->
+        <!-- TODO : gérer type de fichier accept=".pdf,.doc,.docx" -->
     </div>
     <div
       id="text-upload-error-desc-error"
@@ -25,12 +24,36 @@
       >
       {{ error }}
     </div>
+    <div class="fr-grid-row">
+        <div class="fr-col">
+            <progress
+              max="100"
+              id="progress_signalement_photos"
+              :value="uploadPercentage"
+              v-if="uploadPercentage > 0 && uploadPercentage > 100"
+              >
+            </progress>
+        </div>
+    </div>
+    <div class="fr-grid-row">
+        <div class="fr-col fr-text--center">
+          <button
+            id="signalement_uploadedfile_delete"
+            class="fr-mt-2v fr-btn fr-btn--sm fr-btn--danger"
+            v-if="formStore.data[id] !== undefined"
+            @click="deleteFile"
+            >
+            Supprimer
+          </button>
+        </div>
+    </div>
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { requests } from './../requests'
+import formStore from './../store'
 
 export default defineComponent({
   name: 'SignalementFormUpload',
@@ -41,40 +64,71 @@ export default defineComponent({
     modelValue: { type: String, default: null },
     customCss: { type: String, default: '' },
     validate: { type: Object, default: null },
-    hasError: { type: Boolean, default: false },
-    disabled: { type: Boolean, default: false },
-    error: { type: String, default: '' }
+    disabled: { type: Boolean, default: false }
   },
-  computed: {
-    internalValue: {
-      get () {
-        return this.modelValue
-      },
-      set (newValue: string) {
-        this.$emit('update:modelValue', newValue)
-      }
+  data () {
+    return {
+      hasError: false,
+      error: '',
+      uploadPercentage: 0,
+      formStore
     }
   },
   methods: {
-    updateValue (event: Event) {
-      const value = (event.target as HTMLInputElement).value
-      this.$emit('update:modelValue', value)
-    },
     onFileUploaded (requestResponse: any) {
-      console.log(requestResponse)
-      // TODO : gérer l'affichage (erreur ou réussite)
       if (requestResponse) {
-        // TODO : enregistrer l'objet reçu à la place de "C:\\fakepath\\xxx.png" ?
+        if (requestResponse.name === 'AxiosError') {
+          this.hasError = true
+          this.error = requestResponse.message
+        }
+        if (requestResponse.file !== undefined) {
+          this.$emit('update:modelValue', requestResponse)
+          // TODO : on affiche aussi une preview ?
+        }
       }
     },
+    onFileProgress (percentCompleted: any) {
+      this.uploadPercentage = percentCompleted
+    },
     uploadFile (event: Event) {
-      // TODO : afficher une barre de progression ?
       const fileInput = event.target as HTMLInputElement
+      // on supprime la donnée enregistrée jusqu'ici
+      this.$emit('update:modelValue', undefined)
       if (fileInput.files && fileInput.files.length > 0) {
-        const formData = new FormData()
-        formData.append('signalement[documents]', fileInput.files[0])
-        // TODO : faut-il permettre une sélection multiple   ?
-        requests.uploadFile(formData, this.onFileUploaded)
+        if (fileInput.files[0].type === 'image/heic' || fileInput.files[0].type === 'image/heif') {
+          fileInput.value = ''
+          this.hasError = true
+          this.error = 'Les fichiers de format HEIC/HEIF ne sont pas pris en charge, merci de convertir votre image en JPEG ou en PNG avant de l\'envoyer.'
+        } else if (fileInput.files[0].size > 10 * 1024 * 1024) {
+          fileInput.value = ''
+          this.hasError = true
+          this.error = 'L\'image dépasse 10MB'
+        } else {
+          this.hasError = false
+          const formData = new FormData()
+          formData.append('signalement[documents]', fileInput.files[0])
+          // TODO : faut-il permettre une sélection multiple ?
+          requests.uploadFile(formData, this.onFileUploaded, this.onFileProgress)
+        }
+      }
+    },
+    deleteFile (event: Event) {
+      // on supprime la donnée enregistrée jusqu'ici
+      this.$emit('update:modelValue', undefined)
+      // TODO : la supprimer sur le bucket ?
+    }
+  },
+  computed: {
+    uploadedFileTitle () {
+      if (this.formStore.data[this.id] !== undefined) {
+        if (this.formStore.data[this.id].titre !== undefined) {
+          return this.formStore.data[this.id].titre
+        } else {
+          const parsedValue = JSON.parse(this.formStore.data[this.id])
+          return parsedValue.titre
+        }
+      } else {
+        return undefined
       }
     }
   },
