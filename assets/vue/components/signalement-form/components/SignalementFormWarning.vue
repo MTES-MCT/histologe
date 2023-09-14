8fc1dfbfb4a0d7694611388523cb9678e138c5be
emilschn <emilien.schneider@beta.gouv.fr>
2023-09-14 14:12:49 +0200

    fix warning display

--- assets/vue/components/signalement-form/components/SignalementFormWarning.vue
+++ assets/vue/components/signalement-form/components/SignalementFormWarning.vue
@@ -1,7 +1,12 @@
 <template>
-    <div class="fr-alert fr-alert--warning fr-mb-5v">
-        <p class="fr-alert__title" v-html="labelVariablesReplaced"></p>
+  <div class="fr-notice fr-notice--info fr-notice--info-warning fr-mb-5v">
+    <div class="fr-container">
+      <div class="fr-notice__body">
+        <p class="fr-notice__title" v-html="labelVariablesReplaced">
+        </p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -26,4 +31,15 @@ export default defineComponent({
 </script>
 
 <style>
+.fr-notice.fr-notice--info.fr-notice--info-warning {
+  --idle: transparent;
+  --hover: var(--background-contrast-warning-hover);
+  --active: var(--background-contrast-warning-active);
+  background-color: var(--background-contrast-warning);
+  color: var(--text-default-warning);
+}
+.fr-notice.fr-notice--info.fr-notice--info-warning .fr-notice__body::before {
+  -webkit-mask-image: url("../../../../../public/build/dsfr/icons/system/fr--warning-fill.svg");
+  mask-image: url("../../../../../public/build/dsfr/icons/system/fr--warning-fill.svg");
+}
 </style>
