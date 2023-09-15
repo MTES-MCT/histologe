73a11189062d4b66ee6c6574e04b41f673ca9082
emilschn <emilien.schneider@beta.gouv.fr>
2023-09-15 15:02:48 +0200

    truncate file name in upload component

--- assets/vue/components/signalement-form/components/SignalementFormUpload.vue
+++ assets/vue/components/signalement-form/components/SignalementFormUpload.vue
@@ -1,5 +1,5 @@
 <template>
-<div :class="['fr-upload-group', { 'fr-upload-group--disabled': disabled }]" :id="id">
+<div :class="['fr-mb-5v fr-upload-group', { 'fr-upload-group--disabled': disabled }]" :id="id">
   <div :class="[ customCss, 'fr-upload-wrap', 'fr-py-3v' ]">
     <label :for="id" class="fr-btn fr-btn--secondary fr-btn--icon-left fr-icon-add-line">
       {{ label }}
@@ -23,7 +23,7 @@
       :key="index"
       class="fr-grid-row "
       >
-      <div class="fr-col-8">
+      <div class="fr-col-8 cut-text">
         <i>{{ getFileTitle(file) }}</i>
       </div>
       <div class="fr-col-4 fr-signalement-form-upload-delete-button">
@@ -180,5 +180,10 @@ export default defineComponent({
 .fr-signalement-form-upload-delete-button {
   display: flex;
   justify-content: right;
+}
+.fr-upload-group .cut-text {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 }
 </style>
