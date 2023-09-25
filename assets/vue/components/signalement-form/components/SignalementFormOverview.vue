b9952dbeffb78c321c9d940e94e88ff42197b99a
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2023-09-25 16:12:30 +0200

    1589 : complete type and composition

--- assets/vue/components/signalement-form/components/SignalementFormOverview.vue
+++ assets/vue/components/signalement-form/components/SignalementFormOverview.vue
@@ -285,11 +285,36 @@ export default defineComponent({
           }
         }
       }
+      result += this.addLineIfNeeded('composition_logement_superficie', 'Superficie en m² : ')
       result += this.addLineIfNeeded('composition_logement_piece_unique', 'Une seule ou plusieurs pièces ? ')
       if (this.formStore.data.composition_logement_piece_unique === 'plusieurs_pieces') {
         result += this.addLineIfNeeded('composition_logement_nb_pieces', 'Nombre de pièces à vivre : ')
       }
-      result += this.addLineIfNeeded('composition_logement_superficie', 'Superficie en m² : ')
+      for (let i = 1; i <= this.formStore.data.composition_logement_nb_pieces; i++) {
+        result += this.addLineIfNeeded('type_logement_pieces_a_vivre_superficie_piece_' + i, 'Superficie de la pièce ' + i + ' : ')
+        result += this.addLineIfNeeded('type_logement_pieces_a_vivre_hauteur_piece_' + i, 'Hauteur de la pièce ' + i + ' : ')
+      }
+      result += this.addLineIfNeeded('type_logement_commodites_cuisine', 'Cuisine ou coin cuisine ? ')
+      if (this.formStore.data.type_logement_commodites_cuisine === 'non') {
+        result += this.addLineIfNeeded('type_logement_commodites_cuisine_collective', 'Accès à une cuisine collective ? ')
+      } else {
+        result += this.addLineIfNeeded('type_logement_commodites_cuisine_hauteur_plafond', 'La hauteur jusqu\'au plafond est de 2m (200cm) ou plus ? ')
+      }
+      result += this.addLineIfNeeded('type_logement_commodites_salle_de_bain', 'Salle de bain, salle d\'eau avec douche ou baignoire ? ')
+      if (this.formStore.data.type_logement_commodites_salle_de_bain === 'non') {
+        result += this.addLineIfNeeded('type_logement_commodites_salle_de_bain_collective', 'Accès à une salle de bain ou des douches collectives ? ')
+      } else {
+        result += this.addLineIfNeeded('type_logement_commodites_salle_de_bain_hauteur_plafond', 'La hauteur jusqu\'au plafond est de 2m (200cm) ou plus ? ')
+      }
+      result += this.addLineIfNeeded('type_logement_commodites_wc', 'Toilettes (WC) ? ')
+      if (this.formStore.data.type_logement_commodites_wc === 'non') {
+        result += this.addLineIfNeeded('type_logement_commodites_wc_collective', 'Accès à des toilettes (WC) collectives ? ')
+      } else {
+        result += this.addLineIfNeeded('type_logement_commodites_wc_hauteur_plafond', 'La hauteur jusqu\'au plafond est de 2m (200cm) ou plus ? ')
+      }
+      if (this.formStore.data.type_logement_commodites_cuisine === 'oui' && this.formStore.data.type_logement_commodites_wc === 'oui') {
+        result += this.addLineIfNeeded('type_logement_commodites_wc_cuisine', 'Toilettes (WC) et cuisine dans la même pièce ? ')
+      }
       result += this.addLineIfNeeded('composition_logement_nombre_personnes', 'Nombre de personnes : ')
       result += this.addLineIfNeeded('composition_logement_enfants', 'Enfants de moins de 6 ans ? ')
       return result
