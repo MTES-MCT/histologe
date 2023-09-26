9c4eef8a1a04f6ac113da7b4c48640b2e6aaa80c
emilschn <emilien.schneider@beta.gouv.fr>
2023-09-26 17:27:40 +0200

    add button donner votre avis

--- assets/vue/components/signalement-form/components/SignalementFormConfirmation.vue
+++ assets/vue/components/signalement-form/components/SignalementFormConfirmation.vue
@@ -8,6 +8,11 @@
       Bon à savoir : en fonction du type de problèmes rencontrés, la procédure peut prendre du temps.
       Nos services font le maximum pour traiter votre demande au plus vite.
     </p>
+    <p class="fr-text--center">
+      <a href="https://voxusagers.numerique.gouv.fr/Demarches/3104?&amp;view-mode=formulaire-avis&amp;nd_mode=en-ligne-enti%C3%A8rement&amp;nd_source=button&amp;key=8a6945494dbe7dd6e52d3dd28d667c1d">
+        <img src="https://voxusagers.numerique.gouv.fr/static/bouton-bleu.svg" alt="Je donne mon avis">
+      </a>
+    </p>
   </div>
 </template>
 
