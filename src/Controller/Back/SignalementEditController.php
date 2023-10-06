e21e29ac743e973b70ce300897b386074a504dbb
emilschn <emilien.schneider@beta.gouv.fr>
2023-10-06 18:11:35 +0200

    wip edit address bo

---
+++ src/Controller/Back/SignalementEditController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Entity\Signalement;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/bo/signalements')]
+class SignalementEditController extends AbstractController
+{
+    #[Route('/{uuid}/edit-address', name: 'back_signalement_edit_address', methods: 'POST')]
+    public function validationResponseSignalement(
+        Signalement $signalement,
+        Request $request,
+    ): Response {
+        $this->denyAccessUnlessGranted('SIGN_VALIDATE', $signalement);
+        if ($this->isCsrfTokenValid('signalement_edit_address_'.$signalement->getId(), $request->get('_token'))) {
+            // TODO
+
+            $this->addFlash('success', 'Statut du signalement mis à jour avec succés !');
+        } else {
+            $this->addFlash('error', 'Une erreur est survenue...');
+        }
+
+        return $this->redirectToRoute('back_signalement_view', ['uuid' => $signalement->getUuid()]);
+    }
+}
