45ba4a8e68989fd27756c5d333fd9137942b2d73
Saidi <saidi.ahamada@gmail.com>
2023-10-10 13:42:04 +0200

    handle cron esabora sish as clock process

--- src/Command/Clock/ScheduledTaskCommand.php
+++ src/Command/Clock/ScheduledTaskCommand.php
@@ -8,11 +8,14 @@ use Cron\Job\ShellJob;
 use Cron\Report\CronReport;
 use Cron\Resolver\ArrayResolver;
 use Cron\Schedule\CrontabSchedule;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 #[AsCommand(
     name: 'app:scheduled-task',
@@ -20,15 +23,20 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class ScheduledTaskCommand extends Command
 {
+    public function __construct(
+        private ParameterBagInterface $parameterBag,
+        private LoggerInterface $logger,
+    ) {
+        parent::__construct();
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $tasks = [
-            ['command' => 'php bin/console app:sync-esabora-sish', 'schedule' => '*/1 * * * *'],
-            ['command' => 'php bin/console app:sync-esabora-sish-intervention', 'schedule' => '*/2 * * * *'],
-        ];
+        $sleepInterval = $this->parameterBag->get('clock_process')['sleep_interval'];
+        $tasks = $this->parameterBag->get('clock_process')['tasks'];
 
         $io = new SymfonyStyle($input, $output);
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $table = (new Table($output))->setHeaders(['Command', 'Schedule']);
 
         $resolver = new ArrayResolver();
         foreach ($tasks as $task) {
@@ -36,20 +44,21 @@ class ScheduledTaskCommand extends Command
             $job->setCommand($task['command']);
             $job->setSchedule(new CrontabSchedule($task['schedule']));
             $resolver->addJob($job);
+            $table->addRow([$task['command'], $task['schedule']]);
         }
 
         $cron = new Cron();
         $cron->setExecutor(new Executor());
         $cron->setResolver($resolver);
 
-        // Every hour, run the scheduler which will execute the tasks
+        $table->render();
+        // Based on sleepInveral, run the scheduler which will execute the tasks
         // which have to be started at the given minute.
         while (true) {
-            $io->info('[CRON] Running tasks');
+            $this->logger->info('[CRON] Running tasks');
             /** @var CronReport $report */
             $report = $cron->run();
 
-            dump($cron->isRunning());
             while ($cron->isRunning()) {
                 $io->success(sprintf('[CRON] %d tasks has been executed', \count($report->getReports())));
 
@@ -60,7 +69,7 @@ class ScheduledTaskCommand extends Command
                     }
                 }
             }
-            sleep(3600);
+            sleep($sleepInterval);
         }
 
         return Command::SUCCESS;
