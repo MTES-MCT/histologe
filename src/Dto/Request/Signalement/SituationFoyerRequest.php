d3d99403023d64db1134aeaaf761cb814648758a
emilschn <emilien.schneider@beta.gouv.fr>
2023-10-17 10:27:23 +0200

    wip bo save signalement situation foyer

--- src/Dto/Request/Signalement/SituationFoyerRequest.php
+++ src/Dto/Request/Signalement/SituationFoyerRequest.php
@@ -5,6 +5,54 @@ namespace App\Dto\Request\Signalement;
 class SituationFoyerRequest
 {
     public function __construct(
+        private readonly ?string $isLogementSocial = null,
+        private readonly ?string $isRelogement = null,
+        private readonly ?string $isAllocataire = null,
+        private readonly ?string $dateNaissanceOccupant = null,
+        private readonly ?string $numAllocataire = null,
+        private readonly ?string $logementSocialMontantAllocation = null,
+        private readonly ?string $travailleurSocialQuitteLogement = null,
+        private readonly ?string $travailleurSocialAccompagnementDeclarant = null,
     ) {
+    }
+
+    public function getIsLogementSocial(): ?string
+    {
+        return $this->isLogementSocial;
+    }
+
+    public function getIsRelogement(): ?string
+    {
+        return $this->isRelogement;
+    }
+
+    public function getIsAllocataire(): ?string
+    {
+        return $this->isAllocataire;
+    }
+
+    public function getDateNaissanceOccupant(): ?string
+    {
+        return $this->dateNaissanceOccupant;
+    }
+
+    public function getNumAllocataire(): ?string
+    {
+        return $this->numAllocataire;
+    }
+
+    public function getLogementSocialMontantAllocation(): ?string
+    {
+        return $this->logementSocialMontantAllocation;
+    }
+
+    public function getTravailleurSocialQuitteLogement(): ?string
+    {
+        return $this->travailleurSocialQuitteLogement;
+    }
+
+    public function getTravailleurSocialAccompagnementDeclarant(): ?string
+    {
+        return $this->travailleurSocialAccompagnementDeclarant;
     }
 }
