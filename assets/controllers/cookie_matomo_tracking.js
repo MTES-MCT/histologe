63da0c690acc49a02c5aa0772e7c1a866c888d74
Saidi <saidi.ahamada@gmail.com>
2023-11-03 20:06:41 +0100

    fix typo #1772

--- assets/controllers/cookie_matomo_tracking.js
+++ assets/controllers/cookie_matomo_tracking.js
@@ -4,9 +4,9 @@
  * Elle active le suivi de page et le suivi des liens pour Matomo.
  *
  * Ces cookies Matomo sont utilisés en conjonction avec des dimensions personnalisées créées :
- * - Dimension "Rôle" : Utilisée pour suivre les rôles spécifiques de l'utilisateur sur le site.
+ * - Dimension "Rôle" : Utilisée pour suivre le rôle spécifique de l'utilisateur sur le site.
  * - Dimension "Territoire" : Utilisée pour suivre la localisation géographique de l'utilisateur.
- * - Dimension "Partenaire" : Utilisée pour suivre les partenaires ou affiliations de l'utilisateur.
+ * - Dimension "Partenaire" : Utilisée pour suivre le partenaires de l'utilisateur.
  *
  * Veuillez noter que les dimensions personnalisées permettent de segmenter et d'analyser les données de suivi
  * en fonction de ces caractéristiques spécifiques.
