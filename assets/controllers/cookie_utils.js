bd85e3df5ab503482146d938180ee02b542a6873
Saidi <saidi.ahamada@gmail.com>
2023-11-03 22:56:48 +0100

    use let instead #1772

--- assets/controllers/cookie_utils.js
+++ assets/controllers/cookie_utils.js
@@ -13,7 +13,7 @@ export function setCookie(name, value, days) {
 export function getCookie(name) {
     let nameCookie = name + "=";
     let cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
+    for (let i = 0; i < cookies.length; i++) {
         let cookie = cookies[i];
         while (cookie.charAt(0) === " ") {
             cookie = cookie.substring(1, cookie.length);
