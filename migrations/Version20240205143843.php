3a3e656ece0b1768e80b0c984c2296af3e716968
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2024-02-06 15:59:09 +0100

    DocumentType Autre on existing files  #2202

--- migrations/Version20240205143843.php
+++ migrations/Version20240205143843.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
+use App\Entity\Enum\DocumentType;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -17,6 +18,7 @@ final class Version20240205143843 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE file ADD description LONGTEXT DEFAULT NULL');
+        $this->addSql('UPDATE file SET document_type = \''.DocumentType::AUTRE->name.'\' WHERE document_type IS NULL');
     }
 
     public function down(Schema $schema): void
