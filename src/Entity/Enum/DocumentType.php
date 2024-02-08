f074c213846d84c55ac02aa6c406c245493ca86e
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2024-02-08 17:35:28 +0100

    start editing document type #2222

--- src/Entity/Enum/DocumentType.php
+++ src/Entity/Enum/DocumentType.php
@@ -2,8 +2,12 @@
 
 namespace App\Entity\Enum;
 
+use App\Entity\Behaviour\EnumTrait;
+
 enum DocumentType: String
 {
+    use EnumTrait;
+
     case SITUATION_FOYER_BAIL = 'SITUATION_FOYER_BAIL';
     case SITUATION_FOYER_DPE = 'SITUATION_FOYER_DPE';
     case SITUATION_FOYER_ETAT_DES_LIEUX = 'SITUATION_FOYER_ETAT_DES_LIEUX';
@@ -19,28 +23,50 @@ enum DocumentType: String
     case SITUATION = 'SITUATION';
     case VISITE = 'VISITE';
 
-    public function label(): string
+    public static function getLabelList(): array
     {
-        return self::getLabel($this);
+        return [
+            self::SITUATION_FOYER_BAIL->name => 'Bail',
+            self::SITUATION_FOYER_DPE->name => 'DPE',
+            self::SITUATION_FOYER_ETAT_DES_LIEUX->name => 'Etat des lieux',
+            self::SITUATION_DIAGNOSTIC_PLOMB_AMIANTE->name => 'Diagnostic plomb amiante',
+            self::PROCEDURE_MISE_EN_DEMEURE->name => 'Mise en demeure',
+            self::PROCEDURE_RAPPORT_DE_VISITE->name => 'Rapport de visite',
+            self::PROCEDURE_ARRETE_MUNICIPAL->name => 'Arrêté municipal',
+            self::PROCEDURE_ARRETE_PREFECTORAL->name => 'Arrêté préfectoral',
+            self::PROCEDURE_SAISINE->name => 'Saisine',
+            self::BAILLEUR_DEVIS_POUR_TRAVAUX->name => 'Devis pour travaux',
+            self::BAILLEUR_REPONSE_BAILLEUR->name => 'Réponse bailleur',
+            self::AUTRE->name => 'Autre',
+            self::SITUATION->name => 'Photo de désordre',
+            self::VISITE->name => 'Photo de visite',
+        ];
     }
 
-    public static function getLabel(self $value): string
+    public static function getPhotosList(): array
     {
-        return match ($value) {
-            self::SITUATION_FOYER_BAIL => 'Bail',
-            self::SITUATION_FOYER_DPE => 'DPE',
-            self::SITUATION_FOYER_ETAT_DES_LIEUX => 'Etat des lieux',
-            self::SITUATION_DIAGNOSTIC_PLOMB_AMIANTE => 'Diagnostic plomb-amiante',
-            self::PROCEDURE_MISE_EN_DEMEURE => 'Mise en demeure',
-            self::PROCEDURE_RAPPORT_DE_VISITE => 'Rapport de visite',
-            self::PROCEDURE_ARRETE_MUNICIPAL => 'Arrêté municipal',
-            self::PROCEDURE_ARRETE_PREFECTORAL => 'Arrêté préfectoral',
-            self::PROCEDURE_SAISINE => 'Saisine',
-            self::BAILLEUR_DEVIS_POUR_TRAVAUX => 'Devis pour travaux',
-            self::BAILLEUR_REPONSE_BAILLEUR => 'Réponse du bailleur',
-            self::AUTRE => 'Autre',
-            self::SITUATION => 'Désordre',
-            self::VISITE => 'Visite',
-        };
+        return [
+            self::SITUATION->name => self::SITUATION->label(),
+            self::VISITE->name => self::VISITE->label(),
+            self::AUTRE->name => self::AUTRE->label(),
+        ];
+    }
+
+    public static function getDocumentsList(): array
+    {
+        return [
+            self::SITUATION_FOYER_BAIL->name => self::SITUATION_FOYER_BAIL->label(),
+            self::SITUATION_FOYER_DPE->name => self::SITUATION_FOYER_DPE->label(),
+            self::SITUATION_FOYER_ETAT_DES_LIEUX->name => self::SITUATION_FOYER_ETAT_DES_LIEUX->label(),
+            self::SITUATION_DIAGNOSTIC_PLOMB_AMIANTE->name => self::SITUATION_DIAGNOSTIC_PLOMB_AMIANTE->label(),
+            self::PROCEDURE_MISE_EN_DEMEURE->name => self::PROCEDURE_MISE_EN_DEMEURE->label(),
+            self::PROCEDURE_RAPPORT_DE_VISITE->name => self::PROCEDURE_RAPPORT_DE_VISITE->label(),
+            self::PROCEDURE_ARRETE_MUNICIPAL->name => self::PROCEDURE_ARRETE_MUNICIPAL->label(),
+            self::PROCEDURE_ARRETE_PREFECTORAL->name => self::PROCEDURE_ARRETE_PREFECTORAL->label(),
+            self::PROCEDURE_SAISINE->name => self::PROCEDURE_SAISINE->label(),
+            self::BAILLEUR_DEVIS_POUR_TRAVAUX->name => self::BAILLEUR_DEVIS_POUR_TRAVAUX->label(),
+            self::BAILLEUR_REPONSE_BAILLEUR->name => self::BAILLEUR_REPONSE_BAILLEUR->label(),
+            self::AUTRE->name => self::AUTRE->label(),
+        ];
     }
 }
