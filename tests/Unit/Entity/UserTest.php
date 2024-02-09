38946f77b84abcbcebb06c585f73826adcf85002
numew <numabord@gmail.com>
2024-02-09 11:29:11 +0100

    add password validation test

--- tests/Unit/Entity/UserTest.php
+++ tests/Unit/Entity/UserTest.php
@@ -4,9 +4,11 @@ namespace App\Tests\Unit\Entity;
 
 use App\Entity\User;
 use App\Tests\FixturesHelper;
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class UserTest extends TestCase
+class UserTest extends KernelTestCase
 {
     use FixturesHelper;
 
@@ -16,5 +18,46 @@ class UserTest extends TestCase
         $this->assertEquals('John', $user->getPrenom());
         $this->assertEquals('Doe', $user->getNom());
         $this->assertEquals('DOE John', $user->getNomComplet());
+    }
+
+    /**
+     * @dataProvider provideInvalidPassword
+     */
+    public function testPasswordValidationError(string $expectedResult, string $password)
+    {
+        /** @var ValidatorInterface $validator */
+        $validator = static::getContainer()->get(ValidatorInterface::class);
+
+        $user = new User();
+        $user->setPassword($password);
+
+        $errors = $validator->validate($user, null, ['password']);
+
+        /** @var ConstraintViolationList $errors */
+        $errorsAsString = (string) $errors;
+        $this->assertStringContainsString($expectedResult, $errorsAsString);
+    }
+
+    public function testPasswordValidationSuccess()
+    {
+        /** @var ValidatorInterface $validator */
+        $validator = static::getContainer()->get(ValidatorInterface::class);
+
+        $user = new User();
+        $user->setPassword('histologe-H1');
+
+        $errors = $validator->validate($user, null, ['password']);
+
+        $this->assertCount(0, $errors);
+    }
+
+    public function provideInvalidPassword(): \Generator
+    {
+        yield 'blank' => ['Cette valeur ne doit pas être vide', ''];
+        yield 'short' => ['Le mot de passe doit contenir au moins 8 caratères', 'short'];
+        yield 'no_uppercase' => ['Le mot de passe doit contenir au moins une lettre majuscule', 'nouppercase'];
+        yield 'no_lowercase' => ['Le mot de passe doit contenir au moins une lettre minuscule', 'NOLOWERCASE'];
+        yield 'no_digit' => ['Le mot de passe doit contenir au moins un chiffre', 'NoDigitNoDigit'];
+        yield 'no_special' => ['Le mot de passe doit contenir au moins un caractère spécial', 'NoSpecial'];
     }
 }
