555053d45a0c38656fde967c7261a57ff67c40c7
Saidi <saidi.ahamada@gmail.com>
2024-03-18 12:54:48 +0100

    fix test #2215

--- tests/Functional/Service/Signalement/ZipCodeProviderTest.php
+++ tests/Functional/Service/Signalement/ZipCodeProviderTest.php
@@ -7,44 +7,35 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class ZipCodeProviderTest extends KernelTestCase
 {
-    private ZipcodeProvider $zipcodeProvider;
-
-    protected function setUp(): void
-    {
-        self::bootKernel();
-        $container = static::getContainer();
-        $this->zipcodeProvider = $container->get(ZipcodeProvider::class);
-    }
-
     public function testGetZipCodeTerritory(): void
     {
         $this->assertEquals(
             ZipcodeProvider::CORSE_DU_SUD_CODE_DEPARTMENT_2A,
-            $this->zipcodeProvider->getZipCode('20167'),
+            ZipcodeProvider::getZipCode('20167'),
         );
         $this->assertEquals(
             ZipcodeProvider::CORSE_DU_SUD_CODE_DEPARTMENT_2A,
-            $this->zipcodeProvider->getZipCode('20000'),
+            ZipcodeProvider::getZipCode('20000'),
         );
         $this->assertEquals(
             ZipcodeProvider::HAUTE_CORSE_CODE_DEPARTMENT_2B,
-            $this->zipcodeProvider->getZipCode('20200')
+            ZipcodeProvider::getZipCode('20200')
         );
         $this->assertEquals(
             ZipcodeProvider::HAUTE_CORSE_CODE_DEPARTMENT_2B,
-            $this->zipcodeProvider->getZipCode('20600')
+            ZipcodeProvider::getZipCode('20600')
         );
         $this->assertEquals(
             ZipcodeProvider::LA_REUNION_CODE_DEPARTMENT_974,
-            $this->zipcodeProvider->getZipCode('97400')
+            ZipcodeProvider::getZipCode('97400')
         );
         $this->assertEquals(
             ZipcodeProvider::MARTINIQUE_CODE_DEPARTMENT_972,
-            $this->zipcodeProvider->getZipCode('97200')
+            ZipcodeProvider::getZipCode('97200')
         );
         $this->assertEquals(
             '13',
-            $this->zipcodeProvider->getZipCode('13002')
+            ZipcodeProvider::getZipCode('13002')
         );
     }
 }
