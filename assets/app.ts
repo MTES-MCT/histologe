0bde4b803c9060aad3fb2726d1358e33224c2c59
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2024-03-26 18:05:56 +0100

    icones sur les liens externes #2387

--- assets/app.ts
+++ assets/app.ts
@@ -7,7 +7,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/histologe.scss';
-import './styles/tooltip.scss';
 
 import * as Sentry from '@sentry/browser';
 
