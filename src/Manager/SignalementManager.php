b72e25216038fbe7b051da22a8c58e36dea37e8d
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2024-03-27 11:34:18 +0100

    channges based on comments #2198

--- src/Manager/SignalementManager.php
+++ src/Manager/SignalementManager.php
@@ -13,11 +13,9 @@ use App\Dto\Request\Signalement\QualificationNDERequest;
 use App\Dto\Request\Signalement\SituationFoyerRequest;
 use App\Dto\SignalementAffectationListView;
 use App\Entity\Affectation;
-use App\Entity\Enum\DocumentType;
 use App\Entity\Enum\MotifCloture;
 use App\Entity\Enum\ProfileDeclarant;
 use App\Entity\Enum\ProprioType;
-use App\Entity\File;
 use App\Entity\Model\InformationComplementaire;
 use App\Entity\Model\InformationProcedure;
 use App\Entity\Model\SituationFoyer;
@@ -802,62 +800,6 @@ class SignalementManager extends AbstractManager
                 $row
             );
         }
-    }
-
-    public function getPhotosBySlug(Signalement $signalement, string $desordrePrecisionSlug): ?array
-    {
-        $photos = $signalement->getPhotos()->filter(function (File $file) use ($desordrePrecisionSlug) {
-            return DocumentType::PHOTO_SITUATION === $file->getDocumentType()
-            && $file->getDesordreSlug() === $desordrePrecisionSlug;
-        });
-
-        return $photos->toArray();
-    }
-
-    public function getAllPhotosOrdered(Signalement $signalement): ?array
-    {
-        $photoList = $signalement->getPhotos()->toArray();
-        $photoListByType = [
-            DocumentType::PHOTO_SITUATION->value => [],
-            DocumentType::PHOTO_VISITE->value => [],
-            DocumentType::AUTRE->value => [],
-        ];
-
-        foreach ($photoList as $photoItem) {
-            $type = $photoItem->getDocumentType();
-            $photoListByType[$type->value][] = $photoItem;
-        }
-
-        foreach ($photoListByType as &$photoArray) {
-            usort($photoArray, function (File $fileA, File $fileB) {
-                if (DocumentType::PHOTO_SITUATION === $fileA->getDocumentType()) {
-                    return $fileA->getId() <=> $fileB->getId();
-                }
-                if (DocumentType::PHOTO_VISITE === $fileA->getDocumentType()) {
-                    $interventionA = $fileA->getIntervention();
-                    $interventionB = $fileB->getIntervention();
-                    if (null === $interventionA && null === $interventionB) {
-                        return 0;
-                    }
-                    if (null === $interventionA) {
-                        return 1;
-                    }
-                    if (null === $interventionB) {
-                        return -1;
-                    }
-
-                    return $interventionA->getId() <=> $interventionB->getId();
-                }
-
-                return $fileA->getId() <=> $fileB->getId();
-            });
-        }
-
-        return array_merge(
-            $photoListByType[DocumentType::PHOTO_SITUATION->value],
-            $photoListByType[DocumentType::AUTRE->value],
-            $photoListByType[DocumentType::PHOTO_VISITE->value]
-        );
     }
 
     /**
---
+++ tests/Functional/Service/Signalement/PhotoHelperTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Tests\Functional\Service\Signalement;
+
+use App\Entity\Enum\DocumentType;
+use App\Factory\SignalementAffectationListViewFactory;
+use App\Factory\SignalementExportFactory;
+use App\Factory\SignalementFactory;
+use App\Manager\SignalementManager;
+use App\Manager\SuiviManager;
+use App\Repository\BailleurRepository;
+use App\Repository\DesordreCritereRepository;
+use App\Repository\DesordrePrecisionRepository;
+use App\Service\Signalement\CriticiteCalculator;
+use App\Service\Signalement\DesordreTraitement\DesordreCompositionLogementLoader;
+use App\Service\Signalement\PhotoHelper;
+use App\Service\Signalement\Qualification\QualificationStatusService;
+use App\Service\Signalement\Qualification\SignalementQualificationUpdater;
+use App\Specification\Signalement\SuroccupationSpecification;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+
+class PhotoHelperTest extends KernelTestCase
+{
+    private EntityManagerInterface $entityManager;
+    private Security $security;
+    private ManagerRegistry $managerRegistry;
+    private SignalementFactory $signalementFactory;
+    private EventDispatcherInterface $eventDispatcher;
+    private QualificationStatusService $qualificationStatusService;
+    private SignalementAffectationListViewFactory $signalementAffectationListViewFactory;
+    private SignalementExportFactory $signalementExportFactory;
+    private ParameterBagInterface $parameterBag;
+    private SignalementManager $signalementManager;
+    private CsrfTokenManagerInterface $csrfTokenManager;
+    private SuroccupationSpecification $suroccupationSpecification;
+    private CriticiteCalculator $criticiteCalculator;
+    private SignalementQualificationUpdater $signalementQualificationUpdater;
+    private DesordrePrecisionRepository $desordrePrecisionRepository;
+    private DesordreCritereRepository $desordreCritereRepository;
+    private DesordreCompositionLogementLoader $desordreCompositionLogementLoader;
+    private SuiviManager $suiviManager;
+    private BailleurRepository $bailleurRepository;
+
+    protected function setUp(): void
+    {
+        $this->entityManager = static::getContainer()->get('doctrine')->getManager();
+        $this->managerRegistry = static::getContainer()->get(ManagerRegistry::class);
+        $this->security = static::getContainer()->get('security.helper');
+        $this->signalementFactory = static::getContainer()->get(SignalementFactory::class);
+        $this->eventDispatcher = static::getContainer()->get(EventDispatcherInterface::class);
+        /* @var QualificationStatusService $qualificationStatusService */
+        $this->qualificationStatusService = static::getContainer()->get(QualificationStatusService::class);
+        $this->signalementAffectationListViewFactory = static::getContainer()->get(
+            SignalementAffectationListViewFactory::class
+        );
+        $this->signalementExportFactory = static::getContainer()->get(SignalementExportFactory::class);
+        $this->parameterBag = static::getContainer()->get(ParameterBagInterface::class);
+        $this->csrfTokenManager = static::getContainer()->get(CsrfTokenManagerInterface::class);
+        $this->suroccupationSpecification = static::getContainer()->get(SuroccupationSpecification::class);
+        $this->criticiteCalculator = static::getContainer()->get(CriticiteCalculator::class);
+        $this->signalementQualificationUpdater = static::getContainer()->get(SignalementQualificationUpdater::class);
+        $this->desordrePrecisionRepository = static::getContainer()->get(DesordrePrecisionRepository::class);
+        $this->desordreCritereRepository = static::getContainer()->get(DesordreCritereRepository::class);
+        $this->desordreCompositionLogementLoader = static::getContainer()->get(DesordreCompositionLogementLoader::class);
+        $this->suiviManager = static::getContainer()->get(SuiviManager::class);
+        $this->bailleurRepository = static::getContainer()->get(BailleurRepository::class);
+
+        $this->signalementManager = new SignalementManager(
+            $this->managerRegistry,
+            $this->security,
+            $this->signalementFactory,
+            $this->eventDispatcher,
+            $this->qualificationStatusService,
+            $this->signalementAffectationListViewFactory,
+            $this->signalementExportFactory,
+            $this->parameterBag,
+            $this->csrfTokenManager,
+            $this->suroccupationSpecification,
+            $this->criticiteCalculator,
+            $this->signalementQualificationUpdater,
+            $this->desordrePrecisionRepository,
+            $this->desordreCritereRepository,
+            $this->desordreCompositionLogementLoader,
+            $this->suiviManager,
+            $this->bailleurRepository,
+        );
+    }
+
+    public function testGetPhotosBySlug(): void
+    {
+        $signalement = $this->signalementManager->findOneBy(['reference' => '2023-27']);
+
+        $desordrePrecisionSlug = 'desordres_batiment_proprete_interieur';
+        $photos = PhotoHelper::getPhotosBySlug($signalement, $desordrePrecisionSlug);
+        $this->assertCount(1, $photos);
+        $firstKey = array_keys($photos)[0];
+        $this->assertEquals(DocumentType::PHOTO_SITUATION, $photos[$firstKey]->getDocumentType());
+        $this->assertEquals('Capture-d-ecran-du-2023-06-13-12-58-43-648b2a6b9730f.png', $photos[$firstKey]->getTitle());
+
+        $desordrePrecisionSlug = 'desordres_batiment_isolation_murs';
+        $photos = PhotoHelper::getPhotosBySlug($signalement, $desordrePrecisionSlug);
+        $this->assertCount(0, $photos);
+    }
+
+    public function testGetSortedPhotos(): void
+    {
+        $signalement = $this->signalementManager->findOneBy(['reference' => '2023-27']);
+
+        $photos = PhotoHelper::getSortedPhotos($signalement);
+        $this->assertCount(3, $photos);
+        $this->assertEquals(DocumentType::PHOTO_SITUATION, $photos[0]->getDocumentType());
+        $this->assertEquals('Capture-d-ecran-du-2023-06-13-12-58-43-648b2a6b9730f.png', $photos[0]->getTitle());
+        $this->assertEquals(DocumentType::PHOTO_SITUATION, $photos[1]->getDocumentType());
+        $this->assertEquals(DocumentType::AUTRE, $photos[2]->getDocumentType());
+    }
+}
