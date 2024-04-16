33afb1642761b7cb3cf33d43eb75527f28454660
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2024-04-16 12:22:56 +0200

    fix typo #2445 #2448

--- assets/controllers/back_signalement_delete_file/back_signalement_delete_file.js
+++ assets/controllers/back_signalement_delete_file/back_signalement_delete_file.js
@@ -2,7 +2,7 @@ document.querySelectorAll('.btn-signalement-file-delete').forEach(swbtn => {
   swbtn.addEventListener('click', evt => {
     evt.preventDefault()
     const target = evt.target
-    if ( target.getAttribute('data-type') === 'photo') {
+    if (target.getAttribute('data-type') === 'photo') {
       document.querySelectorAll('.fr-modal-file-delete-type').forEach(typeItem => {
         typeItem.innerHTML = 'la photo'
       })
