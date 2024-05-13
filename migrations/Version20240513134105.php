d307034da96ccfef536bca0dae84598850375e69
Saidi <saidi.ahamada@gmail.com>
2024-05-13 15:50:03 +0200

    check if column exists #2482

--- migrations/Version20240513134105.php
+++ migrations/Version20240513134105.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20240426100957 extends AbstractMigration
+final class Version20240513134105 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -16,7 +16,9 @@ final class Version20240426100957 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE file ADD is_temp TINYINT(1) NOT NULL');
+        if (!$schema->getTable('file')->hasColumn('is_temp')) {
+            $this->addSql('ALTER TABLE file ADD is_temp TINYINT(1) NOT NULL');
+        }
     }
 
     public function down(Schema $schema): void
