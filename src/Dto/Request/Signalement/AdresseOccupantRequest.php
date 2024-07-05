3ca536294de7f990117473111ed755bf23f0a669
emilschn <emilien.schneider@beta.gouv.fr>
2024-07-05 15:21:57 +0200

    validate address in bo form #2758

--- src/Dto/Request/Signalement/AdresseOccupantRequest.php
+++ src/Dto/Request/Signalement/AdresseOccupantRequest.php
@@ -8,7 +8,8 @@ class AdresseOccupantRequest implements RequestInterface
 {
     public function __construct(
         #[Assert\NotBlank(message: 'Merci de saisir une adresse.')]
-        #[Assert\Length(max: 100, maxMessage: 'L\'adresse ne peut pas dépasser {{ limit }} caractères.')]
+        #[Assert\Length(min: 6, minMessage: 'L\'adresse n\'est pas au bon format.', max: 100, maxMessage: 'L\'adresse ne peut pas dépasser {{ limit }} caractères.')]
+        #[Assert\Regex('/.*[^0-9].*/')]
         private readonly ?string $adresse = null,
         #[Assert\NotBlank(message: 'Merci de saisir un code postal.')]
         #[Assert\Regex(pattern: '/^[0-9]{5}$/', message: 'Le code postal doit être composé de 5 chiffres.')]
