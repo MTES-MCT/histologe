9e2247a1e71311e33733cbc5ba17d3b5fbe558e9
numew <numabord@gmail.com>
2024-08-01 15:10:55 +0200

    fix deploy #2885

--- migrations/Version20240726144731.php
+++ migrations/Version20240726144731.php
@@ -16,21 +16,15 @@ final class Version20240726144731 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('DROP INDEX `primary` ON desordre_categorie_signalement');
-        $this->addSql('ALTER TABLE desordre_categorie_signalement ADD PRIMARY KEY (signalement_id, desordre_categorie_id)');
-        $this->addSql('DROP INDEX `primary` ON desordre_critere_signalement');
-        $this->addSql('ALTER TABLE desordre_critere_signalement ADD PRIMARY KEY (signalement_id, desordre_critere_id)');
-        $this->addSql('DROP INDEX `primary` ON desordre_precision_signalement');
-        $this->addSql('ALTER TABLE desordre_precision_signalement ADD PRIMARY KEY (signalement_id, desordre_precision_id)');
+        $this->addSql('ALTER TABLE desordre_categorie_signalement DROP PRIMARY KEY, ADD PRIMARY KEY (signalement_id, desordre_categorie_id)');
+        $this->addSql('ALTER TABLE desordre_critere_signalement DROP PRIMARY KEY, ADD PRIMARY KEY (signalement_id, desordre_critere_id)');
+        $this->addSql('ALTER TABLE desordre_precision_signalement DROP PRIMARY KEY, ADD PRIMARY KEY (signalement_id, desordre_precision_id)');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP INDEX `PRIMARY` ON desordre_categorie_signalement');
-        $this->addSql('ALTER TABLE desordre_categorie_signalement ADD PRIMARY KEY (desordre_categorie_id, signalement_id)');
-        $this->addSql('DROP INDEX `PRIMARY` ON desordre_critere_signalement');
-        $this->addSql('ALTER TABLE desordre_critere_signalement ADD PRIMARY KEY (desordre_critere_id, signalement_id)');
-        $this->addSql('DROP INDEX `PRIMARY` ON desordre_precision_signalement');
-        $this->addSql('ALTER TABLE desordre_precision_signalement ADD PRIMARY KEY (desordre_precision_id, signalement_id)');
+        $this->addSql('ALTER TABLE desordre_categorie_signalement DROP PRIMARY KEY, ADD PRIMARY KEY (desordre_categorie_id, signalement_id)');
+        $this->addSql('ALTER TABLE desordre_critere_signalement DROP PRIMARY KEY, ADD PRIMARY KEY (desordre_critere_id, signalement_id)');
+        $this->addSql('ALTER TABLE desordre_precision_signalement DROP PRIMARY KEY, ADD PRIMARY KEY (desordre_precision_id, signalement_id)');
     }
 }
