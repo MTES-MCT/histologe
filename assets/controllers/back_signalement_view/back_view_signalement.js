fa9774815d12d9f8baa7737a6fe0b63545745229
numew <numabord@gmail.com>
2024-08-09 17:06:39 +0200

    fix search tag for add #2922

--- assets/controllers/back_signalement_view/back_view_signalement.js
+++ assets/controllers/back_signalement_view/back_view_signalement.js
@@ -69,7 +69,7 @@ document?.querySelector('.back-link')?.addEventListener('click', (event) => {
 
 document?.querySelectorAll('.signalement-tag-add')?.forEach(element => {
     element.addEventListener('click', (event) => {
-        element.classList?.add('fr-hidden')
+        element.classList?.add('fr-hidden', 'disabled')
 
         const etiquette = document.createElement('span');
         etiquette.classList.add('fr-badge', 'fr-badge--blue-ecume', 'fr-m-1v', 'signalement-tag-remove')
@@ -98,7 +98,7 @@ const inputEtiquetteFilter = document?.querySelector('#etiquette-filter-input')
 inputEtiquetteFilter?.addEventListener('input', (event) => {
     const inputValue = event.target.value.toLowerCase()
     document?.querySelectorAll('.signalement-tag-add')?.forEach(element => {
-        if (element.getAttribute('data-taglabel').toLowerCase().indexOf(inputValue) > -1) {
+        if (element.getAttribute('data-taglabel').toLowerCase().indexOf(inputValue) > -1 && !element.classList.contains('disabled')) {
             element.classList?.remove('fr-hidden')
         } else {
             element.classList?.add('fr-hidden')
@@ -115,7 +115,7 @@ document?.querySelectorAll('.signalement-tag-remove')?.forEach(element => {
 const removeEtiquette = (element) => {
     const tagId = element.getAttribute('data-tagid')
     const etiquetteBadgeAdd = document.querySelector('#etiquette-badge-add-' + tagId);
-    etiquetteBadgeAdd.classList?.remove('fr-hidden')
+    etiquetteBadgeAdd.classList?.remove('fr-hidden', 'disabled');
 
     element.remove();
 
