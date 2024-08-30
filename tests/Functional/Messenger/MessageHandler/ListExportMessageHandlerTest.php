a5cabe8a5b886e99bca4f2354f1b32a961c09f73
emilschn <emilien.schneider@beta.gouv.fr>
2024-08-30 12:16:32 +0200

    add test for list export #2931

---
+++ tests/Functional/Messenger/MessageHandler/ListExportMessageHandlerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Tests\Functional\Messenger\MessageHandler;
+
+use App\Entity\Signalement;
+use App\Messenger\Message\ListExportMessage;
+use App\Messenger\MessageHandler\ListExportMessageHandler;
+use App\Repository\UserRepository;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class ListExportMessageHandlerTest extends WebTestCase
+{
+    public function testHandleGenerateListCsv()
+    {
+        self::bootKernel();
+
+        $container = static::getContainer();
+
+        $messageBus = $container->get(MessageBusInterface::class);
+
+        $userEmail = 'admin-01@histologe.fr';
+
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        /** @var User $user */
+        $user = $userRepository->findOneBy(['email' => $userEmail]);
+        $message = (new ListExportMessage())
+            ->setUserId($user->getId())
+            ->setFormat('csv')
+            ->setFilters([])
+            ->setSelectedColumns([]);
+
+        $messageBus->dispatch($message);
+
+        $transport = $container->get('messenger.transport.async_priority_high');
+        $envelopes = $transport->get();
+        // $this->assertCount(1, $envelopes);
+
+        $handler = $container->get(ListExportMessageHandler::class);
+        $handler($message);
+        // $this->assertEmailCount(1);
+        /** @var NotificationEmail $email */
+        $email = $this->getMailerMessage();
+        $this->assertEmailHtmlBodyContains($email, 'export de la liste des signalements');
+        $this->assertEmailAddressContains($email, 'To', $userEmail);
+    }
+
+    public function testHandleGenerateListXlsWithOptions()
+    {
+        self::bootKernel();
+
+        $container = static::getContainer();
+
+        $messageBus = $container->get(MessageBusInterface::class);
+
+        $userEmail = 'admin-territoire-13-01@histologe.fr';
+
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        /** @var User $user */
+        $user = $userRepository->findOneBy(['email' => $userEmail]);
+        $message = (new ListExportMessage())
+            ->setUserId($user->getId())
+            ->setFormat('xlsx')
+            ->setFilters([])
+            ->setSelectedColumns(['INSEE']);
+
+        $messageBus->dispatch($message);
+
+        $transport = $container->get('messenger.transport.async_priority_high');
+        $envelopes = $transport->get();
+        // $this->assertCount(1, $envelopes);
+
+        $handler = $container->get(ListExportMessageHandler::class);
+        $handler($message);
+        // $this->assertEmailCount(1);
+        /** @var NotificationEmail $email */
+        $email = $this->getMailerMessage();
+        $this->assertEmailHtmlBodyContains($email, 'export de la liste des signalements');
+        $this->assertEmailAddressContains($email, 'To', $userEmail);
+    }
+}
