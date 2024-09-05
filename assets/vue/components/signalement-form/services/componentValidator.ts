fea6d095657e763867a801cc58d3a3df9232a427
emilschn <emilien.schneider@beta.gouv.fr>
2024-09-05 10:39:18 +0200

    fix field validator with null values #2995

--- assets/vue/components/signalement-form/services/componentValidator.ts
+++ assets/vue/components/signalement-form/services/componentValidator.ts
@@ -8,9 +8,9 @@ export const componentValidator = {
 
     let regexPattern
     // s'il y a une valeur, on vérifie si un pattern est requis (ou si c'est un type email)
-    if (value !== undefined && value !== '' && component.type === 'SignalementFormEmailfield') {
+    if (value !== undefined && value !== null && value !== '' && component.type === 'SignalementFormEmailfield') {
       regexPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    } else if (value !== undefined && value !== '' && component.validate?.pattern !== undefined) {
+    } else if (value !== undefined && value !== null && value !== '' && component.validate?.pattern !== undefined) {
       regexPattern = new RegExp(component.validate.pattern)
     }
     if (regexPattern !== undefined) {
@@ -19,14 +19,14 @@ export const componentValidator = {
       }
     }
 
-    if (value !== undefined && value !== '' && component.type === 'SignalementFormPhonefield') {
+    if (value !== undefined && value !== null && value !== '' && component.type === 'SignalementFormPhonefield') {
       const countryCode = formStore.data[componentSlug + '_countrycode'].split(':')[0]
       if (!isValidPhoneNumber(value, countryCode)) {
         formStore.validationErrors[componentSlug] = 'Format invalide'
       }
 
       const valueTelSecond = formStore.data[componentSlug + '_secondaire']
-      if (valueTelSecond !== undefined && valueTelSecond !== '') {
+      if (valueTelSecond !== undefined && valueTelSecond !== null && valueTelSecond !== '') {
         const countryCodeSecond = formStore.data[componentSlug + '_secondaire_countrycode'].split(':')[0]
         if (!isValidPhoneNumber(valueTelSecond, countryCodeSecond)) {
           formStore.validationErrors[componentSlug + '_secondaire'] = 'Format invalide'
@@ -38,7 +38,7 @@ export const componentValidator = {
       this.validateAddress(component)
     }
 
-    if (value !== undefined && value !== '' && component.validate?.maxLength !== undefined) {
+    if (value !== undefined && value !== null && value !== '' && component.validate?.maxLength !== undefined) {
       if (value.length > component.validate?.maxLength) {
         const maxLength: string = component.validate?.maxLength.toString()
         formStore.validationErrors[componentSlug] = 'La valeur dépasse la longueur autorisée ' + maxLength
