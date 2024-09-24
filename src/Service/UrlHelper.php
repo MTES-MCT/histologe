a0ac0956ebe8906e5094f6df8989859eec5e974e
numew <numabord@gmail.com>
2024-09-24 15:43:15 +0200

    keep filters on signalements list after redirect #3074

---
+++ src/Service/UrlHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Service;
+
+class UrlHelper
+{
+    public static function arrayToQueryString(array $params): string
+    {
+        $query = '';
+        foreach ($params as $key => $value) {
+            if (is_array($value)) {
+                foreach ($value as $v) {
+                    $query .= '&'.urlencode($key.'[]').'='.urlencode($v);
+                }
+            } else {
+                $query .= '&'.urlencode($key).'='.urlencode($value);
+            }
+        }
+
+        return preg_replace('/&/', '?', $query, 1);
+    }
+}
---
+++ tests/Unit/Service/UrlHelperTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Unit\Service;
+
+use App\Service\UrlHelper;
+use PHPUnit\Framework\TestCase;
+
+class UrlHelperTest extends TestCase
+{
+    /**
+     * @dataProvider providePartnerType
+     */
+    public function testArrayToQueryString(array $origin, string $result): void
+    {
+        $this->assertEquals(UrlHelper::arrayToQueryString($origin), $result);
+    }
+
+    public function providePartnerType(): \Generator
+    {
+        yield 'empty' => [[], ''];
+        yield 'single' => [['searchTerms' => 'saint médard'], '?searchTerms=saint+m%C3%A9dard'];
+        yield 'multiple' => [['searchTerms' => '2024', 'isImported' => 'oui'], '?searchTerms=2024&isImported=oui'];
+        yield 'nested' => [['status' => 'en_cours', 'etiquettes' => [1424, 479]], '?status=en_cours&etiquettes%5B%5D=1424&etiquettes%5B%5D=479'];
+    }
+}
