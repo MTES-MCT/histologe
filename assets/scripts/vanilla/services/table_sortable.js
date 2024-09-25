e7cfcd01049f73ce4916baa45a11b2530b483d22
emilschn <emilien.schneider@beta.gouv.fr>
2024-09-25 17:37:05 +0200

    fix html table sorting #3083

--- assets/scripts/vanilla/services/table_sortable.js
+++ assets/scripts/vanilla/services/table_sortable.js
@@ -3,19 +3,40 @@ const siblingIndex=e=>{
     let t=0;for(;e=e.previousElementSibling;)t++;return t
 }
 
-const sortTableFunction=e=>function(t){
-    "a"==t.target.tagName.toLowerCase()&&(sortRows(e,siblingIndex(t.target.parentNode)),t.preventDefault())
+const sortTableFunction=table=>function(event){
+    if ("a"==event.target.tagName.toLowerCase()) {
+        sortRows(table,siblingIndex(event.target.parentNode));
+        event.preventDefault();
+    }
 }
-const sortRows=(e,t)=>{
-    let r,a,l,n=e.querySelectorAll("tbody tr"),o="thead th:nth-child("+(t+1)+")",d="td:nth-child("+(t+1)+")",s=e.querySelector(o).classList,i=[],c="",u=!0;
-    for(s&&(s.contains("date")?c="date":s.contains("number")&&(c="number")),a=0;a<n.length;a++)
+const sortRows=(table,index)=>{
+    let r,a,l;
+    let n=table.querySelectorAll("tbody tr");
+    let o="thead th:nth-child("+(index+1)+")";
+    let d="td:nth-child("+(index+1)+")";
+    let classList = table.querySelector(o).classList;
+    let isDesc = classList.contains('desc')
+    if (!isDesc) {
+        classList.add('desc')
+    } else {
+        classList.remove('desc')
+    }
+    let i=[],c="",u=!0;
+    for(classList&&(classList.contains("date")?c="date":classList.contains("number")&&(c="number")),a=0;a<n.length;a++)
         l=n[a].querySelector(d),r=l.innerText,isNaN(r)?u=!1:r=parseFloat(r),i.push({value:r,row:n[a]});
-    ""==c&&u&&(c="number"),"number"==c?(i.sort(sortNumberVal),i=i.reverse()):"date"==c?i.sort(sortDateVal):i.sort(sortTextVal);
-    for(let t=0;t<i.length;t++)e.querySelector("tbody").appendChild(i[t].row)
+    ""==c&&u&&(c="number"),
+    "number"==c?(i.sort(sortNumberVal),i=i.reverse()):"date"==c?i.sort(sortDateVal):i.sort(sortTextVal);
+
+    if (isDesc) {
+        i=i.reverse()
+    }
+    for(let t=0;t<i.length;t++) {
+        table.querySelector("tbody").appendChild(i[t].row)
+    }
 }
 
 const sortNumberVal=(e,t)=>sortNumber(e.value,t.value)
-const sortNumber=(e,t)=>{e-t}
+const sortNumber=(e,t)=>{ return t-e }
 const sortDateVal=(e,t)=>{let r=Date.parse(e.value),a=Date.parse(t.value);return sortNumber(r,a)}
 const sortTextVal=(e,t)=>{let r=(e.value+"").toUpperCase(),a=(t.value+"").toUpperCase();return r<a?-1:r>a?1:0}
 
