c61f3f88185516b85d110d36cff534c3e7ec05a3
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2024-10-08 16:39:20 +0200

    add test #3135

--- tests/Functional/Repository/SuiviRepositoryTest.php
+++ tests/Functional/Repository/SuiviRepositoryTest.php
@@ -34,4 +34,19 @@ class SuiviRepositoryTest extends KernelTestCase
 
         $this->assertStringContainsString('le premier suivi de partenaire 13-01', $firstSuivi->getDescription());
     }
+
+    public function testFindSignalementsForThirdRelance(): void
+    {
+        $result = $this->suiviRepository->findSignalementsForThirdRelance();
+        $this->assertCount(1, $result);
+        $signalementRepository = $this->entityManager->getRepository(Signalement::class);
+        $signalement = $signalementRepository->findOneBy(['id' => $result[0]]);
+        $this->assertEquals('2023-15', $signalement->getReference());
+    }
+
+    public function testCountSignalementNoSuiviAfter3Relances(): void
+    {
+        $result = $this->suiviRepository->countSignalementNoSuiviAfter3Relances();
+        $this->assertEquals(0, $result);
+    }
 }
