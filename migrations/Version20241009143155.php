8ab17791799a0b305876b2f0536043ab07d1686f
Saidi <saidi.ahamada@gmail.com>
2024-10-09 18:36:30 +0200

    fix update request

--- migrations/Version20241009143155.php
+++ migrations/Version20241009143155.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
+use App\Entity\JobEvent;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -17,6 +18,9 @@ final class Version20241009143155 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('DROP INDEX idx_job_event_service ON job_event');
+        $this->addSql("UPDATE job_event SET status = :status_failed WHERE status = 'failure'", [
+            'status_failed' => JobEvent::STATUS_FAILED,
+        ]);
     }
 
     public function down(Schema $schema): void
