11b53c4937b46d77cefeba58479ca29d0acb0a09
numew <numabord@gmail.com>
2024-10-28 14:24:41 +0100

    js-lint #2826

--- assets/scripts/vanilla/controllers/back_zone_view/back_zone_index.js
+++ assets/scripts/vanilla/controllers/back_zone_view/back_zone_index.js
@@ -1,19 +1,19 @@
 const searchZoneForm = document.getElementById('search-zone-form')
 
-if(searchZoneForm){
-    searchZoneForm.querySelectorAll('select').forEach((select) => {
-        select.addEventListener('change', function(){
-            searchZoneForm.submit();
-        });
-    });
-    searchZoneForm.addEventListener('submit', function(){
-        document.getElementById('page').value = 1;
-    });
+if (searchZoneForm) {
+  searchZoneForm.querySelectorAll('select').forEach((select) => {
+    select.addEventListener('change', function () {
+      searchZoneForm.submit()
+    })
+  })
+  searchZoneForm.addEventListener('submit', function () {
+    document.getElementById('page').value = 1
+  })
 }
 
 document.querySelectorAll('.open-modal-zone-delete').forEach((button) => {
-    button.addEventListener('click', (e) => {
-        document.getElementById('fr-modal-zone-delete-zone-name').textContent = e.target.dataset.name
-        document.getElementById('fr-modal-zone-delete-btn-submit').href = e.target.dataset.url
-    })
+  button.addEventListener('click', (e) => {
+    document.getElementById('fr-modal-zone-delete-zone-name').textContent = e.target.dataset.name
+    document.getElementById('fr-modal-zone-delete-btn-submit').href = e.target.dataset.url
+  })
 })
