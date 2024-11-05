5caa65d2651c569f977b39c887f25264fbc7b6f2
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2024-11-05 16:17:39 +0100

    changes based on comments #3219

--- assets/scripts/vue/components/signalement-form/services/variableTester.ts
+++ assets/scripts/vue/components/signalement-form/services/variableTester.ts
@@ -1,5 +1,8 @@
 export const variableTester = {
   isNotEmpty (value: any): boolean {
     return value !== undefined && value !== null && value !== ''
+  },
+  isEmpty (value: any): boolean {
+    return value === undefined || value === null || value === ''
   }
 }
