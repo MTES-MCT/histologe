1dc1379fff2c2e6aeedd9ff8aff8539a48a320ee
emilschn <emilien.schneider@beta.gouv.fr>
2024-11-15 18:01:04 +0100

    wip qualification checkboxes #3105

---
+++ src/Form/Type/SearchCheckboxEnumType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchCheckboxEnumType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'expanded' => true,
+            'multiple' => true,
+            'attr' => ['class' => 'search-checkbox'],
+            'noselectionlabel' => '',
+            'nochoiceslabel' => '',
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return EnumType::class;
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $view->vars['noselectionlabel'] = $options['noselectionlabel'];
+        $view->vars['nochoiceslabel'] = $options['nochoiceslabel'];
+    }
+}
