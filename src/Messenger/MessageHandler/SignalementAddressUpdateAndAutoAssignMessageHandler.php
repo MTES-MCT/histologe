9b2541f40b62736b7703cfba086f8ab12f0cfe8e
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2024-12-13 10:21:19 +0100

    auto-affectation asynchrone oublif fichier #3372

---
+++ src/Messenger/MessageHandler/SignalementAddressUpdateAndAutoAssignMessageHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Messenger\MessageHandler;
+
+use App\Messenger\Message\SignalementAddressUpdateAndAutoAssignMessage;
+use App\Repository\SignalementRepository;
+use App\Service\Signalement\AutoAssigner;
+use App\Service\Signalement\SignalementAddressUpdater;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+class SignalementAddressUpdateAndAutoAssignMessageHandler
+{
+    public function __construct(
+        private readonly SignalementRepository $signalementRepository,
+        private readonly SignalementAddressUpdater $signalementAddressUpdater,
+        private readonly AutoAssigner $autoAssigner,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
+    public function __invoke(SignalementAddressUpdateAndAutoAssignMessage $SignalementAddressUpdateAndAutoAssignMessage): void
+    {
+        try {
+            $signalement = $this->signalementRepository->find($SignalementAddressUpdateAndAutoAssignMessage->getSignalementId());
+            $this->signalementAddressUpdater->updateAddressOccupantFromBanData($signalement);
+            $this->autoAssigner->assign($signalement);
+            $this->entityManager->flush();
+        } catch (\Throwable $exception) {
+            $this->logger->error(
+                sprintf(
+                    'The update from address of the signalement (%s) failed for the following reason : %s',
+                    $SignalementAddressUpdateAndAutoAssignMessage->getSignalementId(),
+                    $exception->getMessage()
+                )
+            );
+        }
+    }
+}
