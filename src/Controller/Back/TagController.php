4d845a0ab6ae8750620c2c5e4e9dbc6475fb8f56
emilschn <emilien.schneider@beta.gouv.fr>
2024-12-13 17:37:43 +0100

    standard list for tags #3376

--- src/Controller/Back/TagController.php
+++ src/Controller/Back/TagController.php
@@ -6,10 +6,11 @@ use App\Entity\Tag;
 use App\Entity\User;
 use App\Form\AddTagType;
 use App\Form\EditTagType;
+use App\Form\SearchTagType;
 use App\Manager\TagManager;
 use App\Repository\TagRepository;
-use App\Repository\TerritoryRepository;
 use App\Service\FormHelper;
+use App\Service\SearchTag;
 use App\Service\Signalement\SearchFilterOptionDataProvider;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -30,48 +31,17 @@ class TagController extends AbstractController
     public function index(
         Request $request,
         TagRepository $tagRepository,
-        TerritoryRepository $territoryRepository,
     ): Response {
-        if ($request->isMethod(Request::METHOD_POST)) {
-            $currentTerritory = $territoryRepository->find((int) $request->request->get('territory'));
-            $search = $request->request->get('search');
-
-            return $this->redirect($this->generateUrl('back_tags_index', [
-                'page' => 1,
-                'territory' => $currentTerritory?->getId(),
-                'search' => $search,
-            ]));
-        }
-
-        $page = $request->get('page') ?? 1;
-
-        /** @var User $user */
-        $user = $this->getUser();
-        if ($this->isGranted('ROLE_ADMIN')) {
-            $currentTerritory = $territoryRepository->find((int) $request->get('territory'));
-        } else {
-            $currentTerritory = $user->getFirstTerritory();
-        }
-        $search = $request->get('search');
-
-        $paginatedTags = $tagRepository->findAllActivePaginated(
-            territory: $currentTerritory,
-            search: $search,
-            page: (int) $page
-        );
-        $totalTags = \count($paginatedTags);
+        [$form, $searchTag, $paginatedTags] = $this->handleSearchTag($request, $tagRepository);
 
         $addForm = $this->createForm(AddTagType::class, null, ['action' => $this->generateUrl('back_tags_add')]);
 
         return $this->render('back/tags/index.html.twig', [
-            'currentTerritory' => $currentTerritory,
-            'territories' => $territoryRepository->findAllList(),
-            'tags' => $paginatedTags,
-            'search' => $search,
-            'total' => $totalTags,
-            'page' => $page,
-            'pages' => (int) ceil($totalTags / self::MAX_LIST_PAGINATION),
+            'form' => $form,
             'addForm' => $addForm,
+            'searchTag' => $searchTag,
+            'tags' => $paginatedTags,
+            'pages' => (int) ceil($paginatedTags->count() / self::MAX_LIST_PAGINATION),
         ]);
     }
 
@@ -161,5 +131,18 @@ class TagController extends AbstractController
         $this->addFlash('error', 'Une erreur est survenue lors de la suppression...');
 
         return $this->redirectToRoute('back_tags_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    private function handleSearchTag(Request $request, TagRepository $tagRepository): array
+    {
+        $searchTag = new SearchTag($this->getUser());
+        $form = $this->createForm(SearchTagType::class, $searchTag);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && !$form->isValid()) {
+            $searchTag = new SearchTag($this->getUser());
+        }
+        $paginatedTags = $tagRepository->findFilteredPaginated($searchTag, self::MAX_LIST_PAGINATION);
+
+        return [$form, $searchTag, $paginatedTags];
     }
 }
---
+++ src/Service/SearchTag.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Territory;
+use App\Entity\User;
+use App\Service\Behaviour\SearchQueryTrait;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class SearchTag
+{
+    use SearchQueryTrait {
+        getUrlParams as getUrlParamsBase;
+    }
+
+    private User $user;
+    #[Assert\Positive(message: 'La page doit être un nombre positif')]
+    private ?int $page = 1;
+    private ?string $queryTag = null;
+    private ?Territory $territory = null;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+        if (!$user->isSuperAdmin() && 1 === count($user->getPartnersTerritories())) {
+            $this->territory = $user->getFirstTerritory();
+        }
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function getPage(): int
+    {
+        if ($this->page < 1) {
+            return 1;
+        }
+
+        return $this->page;
+    }
+
+    public function setPage(?int $page): void
+    {
+        $this->page = $page;
+    }
+
+    public function getQueryTag(): ?string
+    {
+        return $this->queryTag;
+    }
+
+    public function setQueryTag(?string $queryTag): void
+    {
+        $this->queryTag = $queryTag;
+    }
+
+    public function getTerritory(): ?Territory
+    {
+        return $this->territory;
+    }
+
+    public function setTerritory(?Territory $territory): void
+    {
+        $this->territory = $territory;
+    }
+
+    public function getUrlParams(): array
+    {
+        $params = $this->getUrlParamsBase();
+        if (isset($params['territory']) && !$this->getUser()->isSuperAdmin()) {
+            unset($params['territory']);
+        }
+
+        return $params;
+    }
+
+    public function getFiltersToText(): array
+    {
+        $filters = [];
+        if ($this->queryTag) {
+            $filters['Recherche'] = $this->queryTag;
+        }
+        if ($this->territory && $this->user->isSuperAdmin()) {
+            $filters['Territoire'] = $this->territory->getZip().' - '.$this->territory->getName();
+        }
+
+        return $filters;
+    }
+}
