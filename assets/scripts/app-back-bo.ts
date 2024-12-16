f54e608d97ba879a9cf94a1e7ca63ef5c39aeb09
emilschn <emilien.schneider@beta.gouv.fr>
2024-12-16 10:31:16 +0100

    standard filter form for archived partners #3376

--- assets/scripts/app-back-bo.ts
+++ assets/scripts/app-back-bo.ts
@@ -24,6 +24,7 @@ import './vanilla/controllers/back_archived_signalements/back_archived_signaleme
 import './vanilla/controllers/back_auto_affectation_rule/form_auto_affectation_rule.js'
 import './vanilla/controllers/back_partner_index/back_partner_index.js';
 import './vanilla/controllers/back_partner_view/form_partner.js';
+import './vanilla/controllers/back_archived_partner_index/back_archived_partner_index.js';
 import './vanilla/controllers/back_signalement_view/back_view_signalement.js';
 import './vanilla/controllers/back_signalement_view/form_upload_documents.js';
 import './vanilla/controllers/back_signalement_view/input_autocomplete_bailleur.js';
---
+++ src/Service/SearchArchivedPartner.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Territory;
+use App\Entity\User;
+use App\Service\Behaviour\SearchQueryTrait;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class SearchArchivedPartner
+{
+    use SearchQueryTrait {
+        getUrlParams as getUrlParamsBase;
+    }
+
+    private User $user;
+    #[Assert\Positive(message: 'La page doit être un nombre positif')]
+    private ?int $page = 1;
+    private ?string $queryArchivedPartner = null;
+    private ?string $territory = null;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+        if (!$user->isSuperAdmin() && 1 === count($user->getPartnersTerritories())) {
+            $this->territory = $user->getFirstTerritory();
+        }
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function getPage(): int
+    {
+        if ($this->page < 1) {
+            return 1;
+        }
+
+        return $this->page;
+    }
+
+    public function setPage(?int $page): void
+    {
+        $this->page = $page;
+    }
+
+    public function getQueryArchivedPartner(): ?string
+    {
+        return $this->queryArchivedPartner;
+    }
+
+    public function setQueryArchivedPartner(?string $queryArchivedPartner): void
+    {
+        $this->queryArchivedPartner = $queryArchivedPartner;
+    }
+
+    public function getTerritory(): ?string
+    {
+        return $this->territory;
+    }
+
+    public function setTerritory(?string $territory): void
+    {
+        $this->territory = $territory;
+    }
+
+    public function getUrlParams(): array
+    {
+        $params = $this->getUrlParamsBase();
+        if (isset($params['territory']) && !$this->getUser()->isSuperAdmin()) {
+            unset($params['territory']);
+        }
+
+        return $params;
+    }
+}
