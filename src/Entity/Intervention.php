ac2332872bd2974926cb2cfc193ace58c94a4b79
emilschn <emilien.schneider@beta.gouv.fr>
2024-12-18 14:41:02 +0100

    fix datetime for visite in emails #3472

--- src/Entity/Intervention.php
+++ src/Entity/Intervention.php
@@ -9,6 +9,7 @@ use App\Entity\Enum\HistoryEntryEvent;
 use App\Entity\Enum\InterventionType;
 use App\Entity\Enum\ProcedureType;
 use App\Repository\InterventionRepository;
+use App\Service\TimezoneProvider;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
@@ -102,6 +103,25 @@ class Intervention implements EntityHistoryInterface
         $this->scheduledAt = $scheduledAt;
 
         return $this;
+    }
+
+    public function getScheduledAtFormated(): string
+    {
+        if ($this->getScheduledAt()->format('H') > 0) {
+            if ($this->getPartner()) {
+                return $this->getScheduledAt()
+                            ->setTimezone(
+                                new \DateTimeZone($this->getPartner()->getTerritory() ? $this->getPartner()->getTerritory()->getTimezone() : TimezoneProvider::TIMEZONE_EUROPE_PARIS)
+                            )
+                            ->format('d/m/Y à H:i');
+            }
+
+            return $this->getScheduledAt()
+                        ->setTimezone(new \DateTimeZone(TimezoneProvider::TIMEZONE_EUROPE_PARIS))
+                        ->format('d/m/Y à H:i');
+        }
+
+        return $this->getScheduledAt()->format('d/m/Y');
     }
 
     public function getRegisteredAt(): ?\DateTimeImmutable
