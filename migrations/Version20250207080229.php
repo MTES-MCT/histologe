fc9ca4e88c6b8f343f7a551afde0035572142120
Hélène Meneuvrier <hmeneuvrier@gmail.com>
2025-02-07 09:09:57 +0100

    add migration #3488

---
+++ migrations/Version20250207080229.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250207080229 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Delete dernier_bail_at for signamement_qualification';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE signalement_qualification DROP dernier_bail_at');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE signalement_qualification ADD dernier_bail_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+}
