395be48c8bb46181170c3400062705508d2eb521
emilschn <emilien.schneider@beta.gouv.fr>
2025-02-17 16:38:58 +0100

    change contact address #3584

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -154,7 +154,7 @@ class HomepageController extends AbstractController
         '/contact',
         name: 'front_contact',
         methods: ['GET'],
-        defaults: ['show_sitemap' => true]
+        defaults: ['show_sitemap' => false]
     )]
     public function contact(
         ParameterBagInterface $parameterBag,
