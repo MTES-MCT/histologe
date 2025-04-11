0c09323c6de77da0999e67d94343172c9ca96a6f
emilschn <emilien.schneider@beta.gouv.fr>
2025-04-11 17:21:58 +0200

    remove contact classes #3205

--- assets/scripts/app.ts
+++ assets/scripts/app.ts
@@ -13,4 +13,3 @@ import './vanilla/services/file_delete.js';
 import './vanilla/controllers/activate_account/activate_account.js';
 import './vanilla/controllers/front_demande_lien_signalement/front_demande_lien_signalement.js';
 import './vanilla/controllers/front_suivi_signalement/front_suivi_signalement.js';
-import './vanilla/controllers/front_contact/front_contact.js';
