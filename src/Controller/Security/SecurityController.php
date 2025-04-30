0cecdc86809f8413fcf435cfee933485281a0a06
hmeneuvrier <hmeneuvrier@gmail.com>
2025-04-30 14:03:23 +0200

    suivi usager messagerie #4069

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -121,6 +121,7 @@ class SecurityController extends AbstractController
         ?Signalement $signalement = null,
     ): Response {
         $request = Request::createFromGlobals();
+        $expectedToken = hash_hmac('sha256', 'suivi_signalement_ext_file_view'.$signalement?->getUuid().$filename, '$secret');
 
         if (
             !$this->isCsrfTokenValid('suivi_signalement_ext_file_view', $request->get('t'))
