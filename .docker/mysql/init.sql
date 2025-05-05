3a182c7965519c4059b9cc4f0dffb38c86a6bcba
emilschn <emilien.schneider@beta.gouv.fr>
2025-05-05 09:52:27 +0200

    wip rename container#4027

--- .docker/mysql/init.sql
+++ .docker/mysql/init.sql
@@ -1,4 +1,4 @@
-GRANT ALL PRIVILEGES ON *.* TO 'histologe'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'signal_logement'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 -- if make load-data failed execute command:
 -- SET GLOBAL max_allowed_packet=16777216;
