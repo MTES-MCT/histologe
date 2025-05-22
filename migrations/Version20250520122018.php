ff97de3f16765fdef81908f41151d40bdb24456b
emilschn <emilien.schneider@beta.gouv.fr>
2025-05-22 16:23:45 +0200

    migration to init value of reminded and avoid existing ones #3581

--- migrations/Version20250520122018.php
+++ migrations/Version20250520122018.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
+use App\Entity\Enum\SignalementDraftStatus;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -17,6 +18,8 @@ final class Version20250520122018 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE signalement_draft ADD pending_draft_reminded_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+
+        $this->addSql('UPDATE `signalement_draft` s SET `pending_draft_reminded_at` = \'1970-01-01\' WHERE s.status = \''.SignalementDraftStatus::EN_COURS->value.'\' AND s.current_step = \'info_procedure_bail\'');
     }
 
     public function down(Schema $schema): void
