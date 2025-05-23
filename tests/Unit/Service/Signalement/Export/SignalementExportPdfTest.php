e5cb5a4fa0e9994b097ce047df9edd7384870584
emilschn <emilien.schneider@beta.gouv.fr>
2025-05-23 11:49:23 +0200

    order visites by date #4085

--- tests/Unit/Service/Signalement/Export/SignalementExportPdfTest.php
+++ tests/Unit/Service/Signalement/Export/SignalementExportPdfTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Unit\Service\Signalement\Export;
 
+use App\Repository\InterventionRepository;
 use App\Repository\SignalementRepository;
 use App\Service\Signalement\Export\SignalementExportPdfGenerator;
 use Knp\Snappy\Pdf;
@@ -19,15 +20,21 @@ class SignalementExportPdfTest extends KernelTestCase
         $signalementExportPdfGenerator = new SignalementExportPdfGenerator($pdf, $parameterBag);
 
         $twig = static::getContainer()->get(Environment::class);
+        /** @var SignalementRepository $signalementRepository */
         $signalementRepository = static::getContainer()->get(SignalementRepository::class);
 
         $signalement = $signalementRepository->findOneBy(['reference' => '2023-1']);
+
+        /** @var InterventionRepository $interventionRepository */
+        $interventionRepository = static::getContainer()->get(InterventionRepository::class);
+        $visites = $interventionRepository->getOrderedVisitesForSignalement($signalement);
 
         $html = $twig->render('pdf/signalement.html.twig', [
             'listConcludeProcedures' => [],
             'signalement' => $signalement,
             'situations' => [],
             'listQualificationStatusesLabelsCheck' => [],
+            'visites' => $visites,
             'isForUsager' => false,
         ]);
         $options = static::getContainer()->getParameter('export_options');
