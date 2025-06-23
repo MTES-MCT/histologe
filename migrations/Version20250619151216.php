8f01906642ee04b41571b8a5b870915e2f8fe3eb
emilschn <emilien.schneider@beta.gouv.fr>
2025-06-23 10:56:55 +0200

    add migration for history entries #3536

--- migrations/Version20250619151216.php
+++ migrations/Version20250619151216.php
@@ -29,14 +29,72 @@ final class Version20250619151216 extends AbstractMigration
         foreach (self::OLD_STATUS_TO_NEW_STATUS as $newStatus => $oldStatus) {
             $this->addSql('UPDATE `affectation` SET `statut` = \''.$newStatus.'\' WHERE statut = \''.$oldStatus.'\'');
         }
+
+        $queryOld = $this->getQueryUpdateHistoryUp('old');
+        $this->addSql($queryOld);
+
+        $queryNew = $this->getQueryUpdateHistoryUp('new');
+        $this->addSql($queryNew);
+    }
+
+    private function getQueryUpdateHistoryUp($field): string
+    {
+        return "
+            UPDATE history_entry
+            SET changes = JSON_SET(
+                changes,
+                '$.statut.".$field."',
+                CASE JSON_UNQUOTE(JSON_EXTRACT(changes, '$.statut.".$field."'))
+                    WHEN '0' THEN 'NOUVEAU'
+                    WHEN '1' THEN 'EN_COURS'
+                    WHEN '2' THEN 'REFUSE'
+                    WHEN '3' THEN 'FERME'
+                    ELSE JSON_UNQUOTE(JSON_EXTRACT(changes, '$.statut.".$field."'))
+                END
+            )
+            WHERE entity_name LIKE '%Affectation'
+            AND event = 'UPDATE'
+            AND source LIKE '%response%'
+            AND JSON_EXTRACT(changes, '$.statut.".$field."') IS NOT NULL
+            AND JSON_UNQUOTE(JSON_EXTRACT(changes, '$.statut.".$field."')) IN ('0','1','2','3')
+        ";
     }
 
     public function down(Schema $schema): void
     {
+        $queryOld = $this->getQueryUpdateHistoryDown('old');
+        $this->addSql($queryOld);
+
+        $queryNew = $this->getQueryUpdateHistoryDown('new');
+        $this->addSql($queryNew);
+
         foreach (self::OLD_STATUS_TO_NEW_STATUS as $newStatus => $oldStatus) {
             $this->addSql('UPDATE `affectation` SET `statut` = \''.$oldStatus.'\' WHERE statut = \''.$newStatus.'\'');
         }
 
         $this->addSql('ALTER TABLE affectation CHANGE statut statut INT NOT NULL');
+    }
+
+    private function getQueryUpdateHistoryDown($field): string
+    {
+        return "
+            UPDATE history_entry
+            SET changes = JSON_SET(
+                changes,
+                '$.statut.".$field."',
+                CASE JSON_UNQUOTE(JSON_EXTRACT(changes, '$.statut.".$field."'))
+                    WHEN 'NOUVEAU' THEN '0'
+                    WHEN 'EN_COURS' THEN '1'
+                    WHEN 'REFUSE' THEN '2'
+                    WHEN 'FERME' THEN '3'
+                    ELSE JSON_UNQUOTE(JSON_EXTRACT(changes, '$.statut.".$field."'))
+                END
+            )
+            WHERE entity_name LIKE '%Affectation'
+            AND event = 'UPDATE'
+            AND source LIKE '%response%'
+            AND JSON_EXTRACT(changes, '$.statut.".$field."') IS NOT NULL
+            AND JSON_UNQUOTE(JSON_EXTRACT(changes, '$.statut.".$field."')) IN ('NOUVEAU','EN_COURS','REFUSE','FERME')
+        ";
     }
 }
