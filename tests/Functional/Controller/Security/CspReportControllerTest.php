53c432936c3e9e00d85ba7fb2cabee5d64be7c99
Saidi <saidi.ahamada@gmail.com>
2025-07-10 15:53:34 +0200

    disable trace for csp extension report #4323

--- tests/Functional/Controller/Security/CspReportControllerTest.php
+++ tests/Functional/Controller/Security/CspReportControllerTest.php
@@ -2,26 +2,32 @@
 
 namespace App\Tests\Functional\Controller\Security;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class CspReportControllerTest extends WebTestCase
 {
-    public function testCspReportWithValidPayload(): void
-    {
+    /**
+     * @param array<string, mixed> $payload
+     *
+     * @dataProvider provideCspReports
+     */
+    public function testCspReportWithValidPayload(
+        array $payload,
+        bool $shouldReport = false,
+    ): void {
         $client = static::createClient();
 
-        $payload = [
-            'csp-report' => [
-                'blocked-uri' => 'https://malicious-site.com',
-                'violated-directive' => 'script-src',
-                'document-uri' => 'https://histologe.fr',
-                'original-policy' => "default-src 'self'; script-src 'self'",
-                'source-file' => 'https://histologe.fr/js/app.js',
-                'line-number' => 42,
-                'column-number' => 10,
-            ],
-        ];
+        if ($shouldReport) {
+            $loggerMock = $this->createMock(LoggerInterface::class);
+            $loggerMock->expects($this->once())
+                ->method('warning')
+
+                ->with($this->stringContains('CSP Violation'));
+
+            static::getContainer()->set(LoggerInterface::class, $loggerMock);
+        }
 
         $client->request(
             'POST',
@@ -34,6 +40,72 @@ class CspReportControllerTest extends WebTestCase
 
         $this->assertEquals(Response::HTTP_NO_CONTENT, $client->getResponse()->getStatusCode());
         $this->assertEmpty($client->getResponse()->getContent());
+    }
+
+    public static function provideCspReports(): \Generator
+    {
+        yield 'valid payload' => [
+            [
+                'csp-report' => [
+                    'blocked-uri' => 'https://malicious-site.com',
+                    'violated-directive' => 'script-src',
+                    'document-uri' => 'https://histologe.fr',
+                    'original-policy' => "default-src 'self'; script-src 'self'",
+                    'source-file' => 'https://histologe.fr/js/app.js',
+                    'line-number' => 42,
+                    'column-number' => 10,
+                ],
+            ],
+            true,
+        ];
+
+        yield 'payload with extension in source-file (chrome)' => [
+            [
+                'csp-report' => [
+                    'blocked-uri' => 'https://malicious-site.com',
+                    'violated-directive' => 'script-src',
+                    'document-uri' => 'https://histologe.fr',
+                    'source-file' => 'chrome-extension://',
+                ],
+            ],
+            false,
+        ];
+
+        yield 'payload with extension in blocked-uri (xiaomi)' => [
+            [
+                'csp-report' => [
+                    'blocked-uri' => 'https://cdn.alsgp0.fds.api.mi-img.com/instant-web/extension/test.min.js',
+                    'violated-directive' => 'script-src',
+                    'document-uri' => 'https://histologe.fr',
+                    'source-file' => 'https://histologe.fr/js/app.js',
+                ],
+            ],
+            false,
+        ];
+
+        yield 'payload with extension in source-file (safari)' => [
+            [
+                'csp-report' => [
+                    'blocked-uri' => 'https://malicious-site.com',
+                    'violated-directive' => 'script-src',
+                    'document-uri' => 'https://histologe.fr',
+                    'source-file' => 'safari-extension://',
+                ],
+            ],
+            false,
+        ];
+
+        yield 'payload with extension in source-file (firefox)' => [
+            [
+                'csp-report' => [
+                    'blocked-uri' => 'https://malicious-site.com',
+                    'violated-directive' => 'script-src',
+                    'document-uri' => 'https://histologe.fr',
+                    'source-file' => 'moz-extension://',
+                ],
+            ],
+            false,
+        ];
     }
 
     public function testCspReportWithGetMethod(): void
