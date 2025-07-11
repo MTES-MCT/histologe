3a8f30daf539ebf4552e85dc3fb2a9e620bf0e3a
Saidi <saidi.ahamada@gmail.com>
2025-07-11 15:59:56 +0200

    update condition #4286

--- assets/scripts/vanilla/controllers/back_dashboard/back_dashboard.js
+++ assets/scripts/vanilla/controllers/back_dashboard/back_dashboard.js
@@ -3,8 +3,13 @@ import initTabsLoader from './tabs_loader';
 import * as Sentry from '@sentry/browser';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const dashboard = document?.getElementById('dashboard');
-  if (!dashboard && window.location.pathname === '/bo') {
+  let dashboard = document?.getElementById('app-dashboard');
+  if (window.location.pathname !== '/bo/' || dashboard) {
+    return;
+  }
+
+  dashboard = document?.getElementById('dashboard');
+  if (!dashboard && window.location.pathname === '/bo/') {
     const error = "Erreur de chargement, merci de vérifier que l'élément #dashboard existe.";
     console.error(error);
     Sentry.captureException(new Error(error));
