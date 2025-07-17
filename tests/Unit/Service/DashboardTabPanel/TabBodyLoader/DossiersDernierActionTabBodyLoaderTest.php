b02543591257d61aba3363d0145a6d99acaf3d9d
hmeneuvrier <hmeneuvrier@gmail.com>
2025-07-17 12:52:00 +0200

    cs-fix #4274

--- tests/Unit/Service/DashboardTabPanel/TabBodyLoader/DossiersDernierActionTabBodyLoaderTest.php
+++ tests/Unit/Service/DashboardTabPanel/TabBodyLoader/DossiersDernierActionTabBodyLoaderTest.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Service\DashboardTabPanel\TabBodyLoader;
 
 use App\Service\DashboardTabPanel\TabBody;
-use App\Service\DashboardTabPanel\TabBodyType;
-use App\Service\DashboardTabPanel\TabQueryParameters;
 use App\Service\DashboardTabPanel\TabBodyLoader\DossiersDernierActionTabBodyLoader;
+use App\Service\DashboardTabPanel\TabBodyType;
 use App\Service\DashboardTabPanel\TabDataManager;
+use App\Service\DashboardTabPanel\TabQueryParameters;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\SecurityBundle\Security;
-use PHPUnit\Framework\MockObject\MockObject;
 
 class DossiersDernierActionTabBodyLoaderTest extends TestCase
 {
@@ -70,4 +70,4 @@ class DossiersDernierActionTabBodyLoaderTest extends TestCase
         $this->assertSame(42, $data['territory_id']);
         $this->assertSame('back/dashboard/tabs/accueil/_body_derniere_action_dossiers.html.twig', $tabBody->getTemplate());
     }
-} 
+}
