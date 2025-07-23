4a595bcfdc50d9d984d1f906710e09a02d0bcc40
hmeneuvrier <hmeneuvrier@gmail.com>
2025-07-23 21:46:03 +0200

    wip accept affectation modal #4316

---
+++ src/Form/AcceptAffectationType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Count;
+
+class AcceptAffectationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('agents', ChoiceType::class, [
+            'choices' => $options['agents'],
+            'multiple' => true,
+            'expanded' => true,
+            'choice_value' => fn ($value) => (string) $value,
+            // 'required' => true,
+            'label_html' => true,
+            'label' => 'Sélectionnez le(s) agent(s) en charge du dossier',
+            'constraints' => [
+                new Count([
+                    'min' => 1,
+                    'minMessage' => 'Veuillez sélectionner au moins un agent.',
+                ]),
+            ],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired('agents');
+        $resolver->setDefaults([
+            'agents' => [],
+        ]);
+    }
+}
---
+++ src/Manager/UserSignalementSubscriptionManager.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Manager;
+
+use App\Entity\Signalement;
+use App\Entity\User;
+use App\Entity\UserSignalementSubscription;
+use Doctrine\Persistence\ManagerRegistry;
+
+class UserSignalementSubscriptionManager extends AbstractManager
+{
+    public function __construct(ManagerRegistry $managerRegistry, string $entityName = UserSignalementSubscription::class)
+    {
+        parent::__construct($managerRegistry, $entityName);
+    }
+
+    public function createOrGet(
+        User $userToSubscribe,
+        Signalement $signalement,
+        User $createdBy,
+        ?bool $isLegacy = false,
+        ?bool $flush = true,
+    ): UserSignalementSubscription {
+        $subscription = $this->findOneBy(['user' => $userToSubscribe, 'signalement' => $signalement]);
+        if (null === $subscription) {
+            $subscription = (new UserSignalementSubscription())
+            ->setUser($userToSubscribe)
+            ->setSignalement($signalement)
+            ->setCreatedBy($createdBy);
+        }
+        if ($isLegacy) {
+            $subscription->setIsLegacy($isLegacy);
+        }
+
+        $this->save($subscription, $flush);
+
+        return $subscription;
+    }
+}
