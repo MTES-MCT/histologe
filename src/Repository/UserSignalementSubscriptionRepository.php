6d2d8d3e999dc39e645c196e0f28e7248e8d9117
hmeneuvrier <hmeneuvrier@gmail.com>
2025-07-25 17:48:55 +0200

    cs-fix #4316

--- src/Repository/UserSignalementSubscriptionRepository.php
+++ src/Repository/UserSignalementSubscriptionRepository.php
@@ -75,25 +75,25 @@ class UserSignalementSubscriptionRepository extends ServiceEntityRepository
             ->select('s')
             ->innerJoin('s.user', 'u')
             ->innerJoin('u.userPartners', 'up');
-    
+
         if ($signalement) {
             $queryBuilder
                 ->andWhere('s.signalement = :signalement')
                 ->setParameter('signalement', $signalement);
         }
-    
+
         if ($partner) {
             $queryBuilder
                 ->andWhere('up.partner = :partner')
                 ->setParameter('partner', $partner);
         }
-    
+
         $subscriptions = $queryBuilder->getQuery()->getResult();
-    
+
         foreach ($subscriptions as $subscription) {
             $this->_em->remove($subscription);
         }
-    
+
         $this->_em->flush();
     }
 }
