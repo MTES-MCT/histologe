b5ee1ef377ce9284a38d3c88e5bf6eb8c72094d2
emilschn <emilien.schneider@beta.gouv.fr>
2025-08-20 12:24:58 +0200

    fix rebase #4471

--- src/Service/Signalement/SearchFilter.php
+++ src/Service/Signalement/SearchFilter.php
@@ -483,12 +483,6 @@ class SearchFilter
                 ->setParameter('signalement_ids', $signalementIds);
         }
 
-        if (!empty($filters['isMessageWithoutResponse']) && $this->featureNewDashboard) {
-            $signalementIds = $this->suiviRepository->getSignalementsIdWithSuivisUsagerOrPoursuiteWithAskFeedbackBefore($user, null);
-            $qb->andWhere('s.id IN (:signalement_ids)')
-                ->setParameter('signalement_ids', $signalementIds);
-        }
-
         return $qb;
     }
 
