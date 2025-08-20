10482774b211efdf958801b57a0671a233180034
hmeneuvrier <hmeneuvrier@gmail.com>
2025-08-20 13:28:19 +0200

    fix tests #4277

--- tests/Unit/Service/DashboardTabPanel/Kpi/TabCountKpiBuilderTest.php
+++ tests/Unit/Service/DashboardTabPanel/Kpi/TabCountKpiBuilderTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Service\DashboardTabPanel\Kpi;
 use App\Entity\User;
 use App\Repository\SignalementRepository;
 use App\Repository\SuiviRepository;
+use App\Service\DashboardTabPanel\Kpi\CountAfermer;
 use App\Service\DashboardTabPanel\Kpi\CountDossiersMessagesUsagers;
 use App\Service\DashboardTabPanel\Kpi\CountNouveauxDossiers;
 use App\Service\DashboardTabPanel\Kpi\TabCountKpi;
@@ -50,12 +51,19 @@ class TabCountKpiBuilderTest extends TestCase
 
         $countNouveaux = new CountNouveauxDossiers(1, 2, 3, 4);
         $countMessages = new CountDossiersMessagesUsagers(1, 2, 3);
+        $countAfermer = new CountAfermer(1, 1, 1);
 
         $this->signalementRepository
             ->expects($this->once())
             ->method('countNouveauxDossiersKpi')
             ->with($territories)
             ->willReturn($countNouveaux);
+
+        $this->signalementRepository
+        ->expects($this->once())
+        ->method('countAllDossiersAferme')
+        ->with($user, $territoryId)
+        ->willReturn($countAfermer);
 
         $this->suiviRepository
             ->expects($this->once())
@@ -72,6 +80,7 @@ class TabCountKpiBuilderTest extends TestCase
         $this->assertInstanceOf(TabCountKpi::class, $tabCountKpi);
         $this->assertSame(10, $tabCountKpi->countNouveauxDossiers);
         $this->assertSame(6, $tabCountKpi->countDossiersMessagesUsagers);
+        $this->assertSame(3, $tabCountKpi->countDossiersAFermer);
     }
 
     public function testWithTabCountKpiForNonAdmin(): void
@@ -91,6 +100,7 @@ class TabCountKpiBuilderTest extends TestCase
 
         $countNouveaux = new CountNouveauxDossiers(1, 2, 1, 2);
         $countMessages = new CountDossiersMessagesUsagers(1, 2, 2);
+        $countAfermer = new CountAfermer(1, 0, 0);
 
         $this->signalementRepository
             ->expects($this->once())
@@ -104,6 +114,12 @@ class TabCountKpiBuilderTest extends TestCase
             ->with($user, $territoryId, $mesDossiers)
             ->willReturn($countMessages);
 
+        $this->signalementRepository
+        ->expects($this->once())
+        ->method('countAllDossiersAferme')
+        ->with($user, $territoryId)
+        ->willReturn($countAfermer);
+
         $tabCountKpi = $this->tabCountKpiBuilder
             ->setTerritories($territories, $territoryId)
             ->setMesDossiers($mesDossiers)
@@ -112,5 +128,6 @@ class TabCountKpiBuilderTest extends TestCase
 
         $this->assertSame(6, $tabCountKpi->countNouveauxDossiers);
         $this->assertSame(5, $tabCountKpi->countDossiersMessagesUsagers);
+        $this->assertSame(1, $tabCountKpi->countDossiersAFermer);
     }
 }
