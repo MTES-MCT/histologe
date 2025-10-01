658af9b616159cc7774a8962459cdbc21232560b
hmeneuvrier <hmeneuvrier@gmail.com>
2025-10-01 12:05:57 +0200

    factorise formType #4640

---
+++ src/Dto/AgentsSelection.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Dto;
+
+use App\Entity\Affectation;
+use App\Entity\User;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class AgentsSelection
+{
+    #[Assert\NotNull(message: 'Affectation manquante.')]
+    private ?Affectation $affectation = null;
+
+    /** @var array<User> */
+    #[Assert\Count(min: 1, minMessage: 'Veuillez sélectionner au moins un agent.')]
+    private array $agents = [];
+
+    public function getAffectation(): ?Affectation
+    {
+        return $this->affectation;
+    }
+
+    public function setAffectation(Affectation $affectation): self
+    {
+        $this->affectation = $affectation;
+
+        return $this;
+    }
+
+    /** @return array<User> */
+    public function getAgents(): array
+    {
+        return $this->agents;
+    }
+
+    /**
+     * @param array<User> $agents
+     */
+    public function setAgents(array $agents): self
+    {
+        $this->agents = $agents;
+
+        return $this;
+    }
+}
---
+++ src/Form/AgentsSelectionType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Form;
+
+use App\Dto\AgentsSelection;
+use App\Entity\Affectation;
+use App\Entity\Enum\UserStatus;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AgentsSelectionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        /** @var AgentsSelection $AgentsSelection */
+        $AgentsSelection = $builder->getData();
+        /** @var Affectation $affectation */
+        $affectation = $AgentsSelection->getAffectation();
+        $choicesAgents = $affectation->getPartner()->getUsers();
+
+        if ($options['exclude_user'] instanceof User) {
+            $choicesAgents = $choicesAgents->filter(fn (User $u) => $u->getId() !== $options['exclude_user']->getId());
+        }
+
+        $builder->add('agents', EntityType::class, [
+            'class' => User::class,
+            'choices' => $choicesAgents,
+            'choice_label' => fn (User $user) => $this->getAgentLabel($user),
+            'multiple' => true,
+            'expanded' => true,
+            'label_html' => true,
+            'label' => $options['label'],
+        ]);
+    }
+
+    private function getAgentLabel(User $agent): string
+    {
+        $html = $agent->getNomComplet(true).' ('.$agent->getEmail().')';
+        if (UserStatus::INACTIVE === $agent->getStatut()) {
+            $html .= '<small class="fr-hint-text fr-text--text-default-warning">';
+            $html .= '<span class="fr-icon--sm fr-icon-warning-line" aria-hidden="true"></span> '.$agent->getRoleLabel().' -  Compte inactif';
+            $html .= '</small>';
+        } else {
+            $html .= '<small class="fr-hint-text fr-text-default--grey">'.$agent->getRoleLabel().'</small>';
+        }
+
+        return $html;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'agents_selection';
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AgentsSelection::class,
+            'exclude_user' => null,
+            'label' => 'Sélectionnez le(s) agent(s) en charge du dossier',
+        ]);
+    }
+}
