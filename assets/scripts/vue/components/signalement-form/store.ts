0f3817cd69ebb9b5d3c7beb49f193831b9315e0f
numew <numabord@gmail.com>
2025-10-03 16:36:14 +0200

    add mediation bailleur feature flipping and new screen in form FO #4697

--- assets/scripts/vue/components/signalement-form/store.ts
+++ assets/scripts/vue/components/signalement-form/store.ts
@@ -91,7 +91,9 @@ const formStore: FormStore = reactive({
     ajaxurlSendMailContinueFromDraft: '',
     ajaxurlSendMailGetLienSuivi: '',
     ajaxurlArchiveDraft: '',
-    initProfile: ''
+    initProfile: '',
+    featureMediationBailleur: '',
+    featureMediationBailleurDepts: ''
   },
   screenData: [],
   currentScreen: null,
@@ -238,7 +240,24 @@ const formStore: FormStore = reactive({
     const dateBailleurPrevenu = new Date(dateBailleurPrevenuSplit[1], dateBailleurPrevenuSplit[0])
     
     return dateToday.getMonth() - dateBailleurPrevenu.getMonth() + (12 * (dateToday.getFullYear() - dateBailleurPrevenu.getFullYear()))
-  }
+  },
+  shouldMediationBailleurSuggested(): boolean {
+      if(!this.props.featureMediationBailleur) {
+        return false
+      }
+      if(!this.props.featureMediationBailleurDepts.includes(formStore.data.territoryCode)) {
+        return false
+      }
+      if(formStore.data.signalement_concerne_logement_social_autre_tiers === 'oui') {
+        return false
+      }
+      //logiquement on devrait aussi vérifier que l'email ou l'adresse du bailleur est renseignée
+      //mais lors de l'appel de cette méthode (écran de saisie des coordonnées du bailleur) on ne sait pas encore ce qui sera renseigné
+      //est-ce que ca remet en question le fait de faire cette vérification ici ?
+      //on pourrait éventuellement proposer cette écran aprés info_procedure_bail pour contourner le problème
+
+      return true
+    },
 })
 
 export default formStore
