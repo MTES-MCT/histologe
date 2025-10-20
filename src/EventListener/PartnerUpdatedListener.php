b9dd530ee68ee0584c5a8d0abe09a8cdb62df4c4
Saidi <saidi.ahamada@gmail.com>
2025-10-20 16:01:33 +0200

    fix tests #4756

--- src/EventListener/PartnerUpdatedListener.php
+++ src/EventListener/PartnerUpdatedListener.php
@@ -3,41 +3,23 @@
 namespace App\EventListener;
 
 use App\Entity\Partner;
-use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
-use Doctrine\ORM\Event\PostFlushEventArgs;
-use Doctrine\ORM\Event\PostUpdateEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
 
-#[AsEntityListener(event: Events::postUpdate, method: 'postUpdate', entity: Partner::class)]
-#[AsDoctrineListener(event: Events::postFlush)]
+#[AsEntityListener(event: Events::preUpdate, method: 'preUpdate', entity: Partner::class)]
 class PartnerUpdatedListener
 {
-    private mixed $entityToFlush = null;
-
-    public function postUpdate(Partner $partner, PostUpdateEventArgs $event): void
+    public function preUpdate(Partner $partner, PreUpdateEventArgs $args): void
     {
-        $unitOfWork = $event->getObjectManager()->getUnitOfWork();
-
-        foreach ($unitOfWork->getEntityChangeSet($partner) as $key => $change) {
-            if ('email' === $key) {
-                $partner->setEmailDeliveryIssue(null);
-                $this->entityToFlush = $partner;
-                break;
-            }
-        }
-    }
-
-    public function postFlush(PostFlushEventArgs $event): void
-    {
-        if (!$this->entityToFlush instanceof Partner) {
+        if (!$args->hasChangedField('email')) {
             return;
         }
 
-        $em = $event->getObjectManager();
-        $em->persist($this->entityToFlush);
+        if (null === $partner->getEmailDeliveryIssue()) {
+            return;
+        }
 
-        $this->entityToFlush = null;
-        $em->flush();
+        $partner->setEmailDeliveryIssue(null);
     }
 }
