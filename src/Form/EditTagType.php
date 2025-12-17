89459b71ce88a42ab8fa89d45e829884eff61d94
numew <numabord@gmail.com>
2025-12-17 14:22:50 +0100

    manage tag edition an deletion without page reloading #4637

--- src/Form/EditTagType.php
+++ src/Form/EditTagType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,13 +19,22 @@ class EditTagType extends AbstractType
             ->add('label', null, [
                 'label' => 'Etiquette',
                 'empty_data' => '',
+            ])
+            ->add('search_params', HiddenType::class, [
+                'mapped' => false,
             ]);
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return '';
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Tag::class,
+            'csrf_token_id' => 'edit_tag',
         ]);
     }
 }
