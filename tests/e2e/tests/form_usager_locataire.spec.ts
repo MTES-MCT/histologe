fd5e5cf773ea971d650cb730ff2cdec4eceea1c2
Saidi <sfinx13@users.noreply.github.com>
2026-01-05 10:39:16 +0100

    [QA] Corriger tests e2e (#5193)
    * fix tests e2e #5192
    
    * update env for playwright action #5192
    
    * remove useless intruction #5192

--- tests/e2e/tests/form_usager_locataire.spec.ts
+++ tests/e2e/tests/form_usager_locataire.spec.ts
@@ -51,6 +51,9 @@ test('signalement form for locataire', async ({page}) => {
     await page.getByRole('textbox', { name: 'Quand avez-vous prévenu votre' }).press('Tab');
     await page.getByRole('textbox', { name: 'Quelle a été la réponse de' }).fill('Demain c\'est loin');
     await page.getByRole('button', { name: 'Suivant' }).click();
+    await page.getByRole('heading', { name: 'Bénéficiez d’une démarche acc' }).click();
+    await page.getByText('Non', { exact: true }).click();
+    await page.getByRole('button', { name: 'Suivant' }).click();
     await page.getByText('Le bâtiment (et / ou parties').click();
     await page.locator('#zone_concernee_debut_desordres-select').selectOption('less_1_month');
     await page.getByRole('button', { name: 'Suivant' }).click();
--- tests/e2e/tests/login.spec.ts
+++ tests/e2e/tests/login.spec.ts
@@ -55,20 +55,20 @@ test('login for admin', async ({page, context}) => {
   await page.goto(`${process.env.BASE_URL ?? 'http://localhost:8080'}/connexion`);
   await page.waitForLoadState('networkidle');
 
-  await page.getByRole('button', { name: 'Connexion' }).click();
+  await page.getByRole('button', { name: 'Se connecter' }).click();
   await page.getByText('Connexion impossible').click();
   await page.getByRole('textbox', { name: 'Courriel Adresse utilisée' }).click();
   await page.getByRole('textbox', { name: 'Courriel Adresse utilisée' }).fill('bla');
-  await page.getByRole('button', { name: 'Connexion' }).click();
+  await page.getByRole('button', { name: 'Se connecter' }).click();
   await page.getByText('Veuillez saisir votre mot de').click();
   await page.getByRole('textbox', { name: 'Courriel Adresse utilisée' }).fill('bla');
   await page.getByRole('textbox', { name: 'Mot de passe Mot de passe dé' }).fill('bla');
-  await page.getByRole('button', { name: 'Connexion' }).click();
+  await page.getByRole('button', { name: 'Se connecter' }).click();
   await page.getByText('Identifiants invalides.').click();
   await page.getByText('Adresse utilisée lors de la').click();
   await page.getByRole('textbox', { name: 'Courriel Adresse utilisée' }).fill('admin-01@signal-logement.fr');
   await page.getByRole('textbox', { name: 'Mot de passe Mot de passe dé' }).fill('signallogement');
-  await page.getByRole('button', { name: 'Connexion' }).click();
+  await page.getByRole('button', { name: 'Se connecter' }).click();
 
   await page.waitForURL('**/bo/**', { timeout: 10000 });
 
