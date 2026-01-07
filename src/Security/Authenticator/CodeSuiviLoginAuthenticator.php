928232103185d02fbdf10477eb5eb4ea9f943943
emilschn <emilien.schneider@beta.gouv.fr>
2026-01-07 15:50:38 +0100

    cs-fix #5189

--- src/Security/Authenticator/CodeSuiviLoginAuthenticator.php
+++ src/Security/Authenticator/CodeSuiviLoginAuthenticator.php
@@ -8,7 +8,6 @@ use App\Manager\UserManager;
 use App\Repository\SignalementRepository;
 use App\Security\Provider\SignalementUserProvider;
 use App\Security\User\SignalementUser;
-use App\Service\RequestDataExtractor;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
