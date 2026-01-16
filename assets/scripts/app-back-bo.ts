9b1ae80a0ec059731e48c2832f2824c50609eb0b
Emilien Schneider <emilien.schneider@poucr.net>
2026-01-16 10:22:46 +0100

    [BO - Listes] Mise en place du responsive pour les tableaux de listes (#5232)
    * wip responsive tables #5199
    
    * responsive data in columns #5199
    
    * wip finish display of responsive tables #5199
    
    * general responsive tables #5199
    
    * replace with nomcomplet #5199

--- assets/scripts/app-back-bo.ts
+++ assets/scripts/app-back-bo.ts
@@ -5,18 +5,19 @@ import './vue/signalement-list.ts';
 import './vue/signalement-carto.ts';
 
 import './vanilla/services/bo_modales.js';
-import './vanilla/services/deprecated/table_sortable.js'
 import './vanilla/services/component/component_search_checkbox.js';
 import './vanilla/services/component/component_select_all_checkbox.js';
 import './vanilla/services/component/component_search_autocomplete.js';
 import './vanilla/services/component/component_phone_number_row.js';
 import './vanilla/services/component/component_search_and_select_badges.js';
+import './vanilla/services/deprecated/table_sortable.js'
 import './vanilla/services/form/ajax_form_handler.js';
 import './vanilla/services/ui/list_filter_helper.js';
-import './vanilla/services/ui/notice_helper.js';
-import './vanilla/services/ui/modales_helper.js';
-import './vanilla/services/ui/tabs_manager.js';
 import './vanilla/services/ui/metabase_resizer.js';
+import './vanilla/services/ui/modales_helper.js';
+import './vanilla/services/ui/notice_helper.js';
+import './vanilla/services/ui/responsive_array.js';
+import './vanilla/services/ui/tabs_manager.js';
 
 import './vanilla/controllers/form_account.js';
 import './vanilla/controllers/form_nde.js';
