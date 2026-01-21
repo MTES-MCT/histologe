62dca19fa30ddfc103238d082bbb48b330c0c8cc
Saidi <sfinx13@users.noreply.github.com>
2026-01-21 10:38:42 +0100

    prevent multi submit #5248 (#5316)

--- assets/scripts/app.ts
+++ assets/scripts/app.ts
@@ -15,4 +15,5 @@ import './vanilla/services/file/file_delete.js';
 import './vanilla/controllers/activate_account/activate_account.js';
 import './vanilla/controllers/front_demande_lien_signalement/front_demande_lien_signalement.js';
 import './vanilla/controllers/front_suivi_signalement/front_suivi_signalement.js';
+import './vanilla/controllers/front_suivi_signalement/front_suivi_signalement_message.js';
 import './vanilla/controllers/front_dossier_bailleur/front_dossier_bailleur.js';
