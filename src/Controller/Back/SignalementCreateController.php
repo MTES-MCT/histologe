6351ede7c6fbc66c7a6f02e58627ccc516e1652c
Saidi <saidi.ahamada@gmail.com>
2026-01-22 17:35:57 +0100

    remove condition #5256

--- src/Controller/Back/SignalementCreateController.php
+++ src/Controller/Back/SignalementCreateController.php
@@ -576,10 +576,6 @@ class SignalementCreateController extends AbstractController
         if (!$signalement->getAdresseOccupant()) {
             $errorMsgs[] = 'Vous devez renseigner l\'adresse du logement pour pouvoir soumettre le signalement.';
         }
-
-        if (null === $signalement->getIsLogementSocial() && null === $signalement->getSituationFoyer()->getLogementSocialAllocation()) {
-            $errorMsgs[] = 'Vous devez renseigner le champ logement social pour pouvoir soumettre le signalement.';
-        }
         if (null === $signalement->getProfileDeclarant()) {
             $errorMsgs[] = 'Vous devez renseigner le profil du déclarant pour pouvoir soumettre le signalement.';
         }
