7f9f77398f43fe766fa03c0f08fbd2dd4f9c19e9
numew <numabord@gmail.com>
2026-01-22 11:13:05 +0100

    [Formulaire service secours] Socle technique (#5272)
    * socle du socle #5231
    
    * Revue/tests [Formulaire service secours] Socle technique (#5305)
    
    * apply some tests #5231
    
    * dont catch profiler route #5231
    
    * #5231 changes based on comments
    
    ---------
    
    Co-authored-by: Saidi <sfinx13@users.noreply.github.com>

---
+++ src/Controller/ServiceSecours/ServiceSecoursController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller\ServiceSecours;
+
+use App\Dto\Api\Request\SignalementRequest;
+use App\Entity\Enum\ProfileDeclarant;
+use App\Form\ServiceSecoursType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(
+    path : '/',
+    requirements: ['domain' => '[^.]+(?:\.[^.]+)*'],
+    host: 'service-secours.{domain}',
+    priority: 100
+)
+]
+class ServiceSecoursController extends AbstractController
+{
+    #[Route('/',
+        name: 'service_secours_index',
+        methods: ['GET', 'POST'])
+    ]
+    public function index(Request $request): Response
+    {
+        $signalementRequest = new SignalementRequest();
+        $signalementRequest->profilDeclarant = ProfileDeclarant::SERVICE_SECOURS->value;
+
+        $form = $this->createForm(ServiceSecoursType::class, $signalementRequest);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // TODO : voir les traitements fait dans App\Controller\Api\SignalementCreateController.php pour les adapter / refactoriser ici
+        }
+
+        return $this->render('service_secours/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route(
+        '/{any}',
+        name: 'service_secours_fallback',
+        requirements: ['any' => '(?!_wdt/|_profiler/|csp-report).*'],
+    )]
+    public function fallback(): Response
+    {
+        throw $this->createNotFoundException();
+    }
+}
