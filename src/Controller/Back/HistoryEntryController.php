e729dea8d8721995724f7efd9c5251d9e93fce2c
numew <numabord@gmail.com>
2026-02-03 13:49:12 +0100

    ajout d'une page SA de visualisation de l'historique des changement d'une entité #5358

--- src/Controller/Back/HistoryEntryController.php
+++ src/Controller/Back/HistoryEntryController.php
@@ -3,11 +3,13 @@
 namespace App\Controller\Back;
 
 use App\Manager\HistoryEntryManager;
+use App\Repository\HistoryEntryRepository;
 use App\Repository\SignalementRepository;
 use App\Security\Voter\SignalementVoter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/bo/history')]
 class HistoryEntryController extends AbstractController
@@ -30,5 +32,21 @@ class HistoryEntryController extends AbstractController
         $historyEntries = $historyEntryManager->getAffectationHistory($signalement);
 
         return $this->json(['historyEntries' => $historyEntries]);
+    }
+
+    #[Route('/{entity_id}/{entity_name}', name: 'back_history_entry_details', methods: ['GET'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function historyEntryDetails(
+        string $entity_id,
+        string $entity_name,
+        HistoryEntryRepository $historyEntryRepository,
+    ): Response {
+        $historyEntries = $historyEntryRepository->findBy(['entityId' => $entity_id, 'entityName' => 'App\\Entity\\'.$entity_name], ['createdAt' => 'ASC']);
+
+        return $this->render('back/history-entry/details.html.twig', [
+            'entityId' => $entity_id,
+            'entityName' => $entity_name,
+            'historyEntries' => $historyEntries,
+        ]);
     }
 }
