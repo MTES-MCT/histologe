597c81c290a1bd03850e77dcfb31b629cb75605f
Saidi <saidi.ahamada@gmail.com>
2026-03-02 18:29:31 +0100

    sanitize error mail #5506

---
+++ tests/Unit/Service/Mailer/Error/ErrorSignalementMailerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests\Unit\Service\Mailer\Error;
+
+use App\Service\Mailer\Mail\Error\ErrorSignalementMailer;
+use PHPUnit\Framework\TestCase;
+
+class ErrorSignalementMailerTest extends TestCase
+{
+    /**
+     * @throws \ReflectionException
+     */
+    public function testSanitizeFoundContent(): void
+    {
+        $mailer = $this->createMock(ErrorSignalementMailer::class);
+
+        $reflection = new \ReflectionClass($mailer);
+        $method = $reflection->getMethod('sanitizeContent');
+
+        $rawPayload = json_encode([
+            'password' => 'secret',
+            'password-current' => 'secret',
+            'password-repeat' => 'secret',
+        ]);
+
+        $result = json_decode($method->invoke($mailer, $rawPayload), true);
+
+        $countFiltered = array_count_values($result)['[Filtered]'] ?? 0;
+        $countSecret = array_count_values($result)['secret'] ?? 0;
+        $this->assertEquals(0, $countSecret);
+        $this->assertEquals(3, $countFiltered);
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    public function testSanitizeNotFoundContent(): void
+    {
+        $mailer = $this->createMock(ErrorSignalementMailer::class);
+
+        $reflection = new \ReflectionClass($mailer);
+        $method = $reflection->getMethod('sanitizeContent');
+
+        $rawPayload = 'Hello world';
+
+        $result = $method->invoke($mailer, $rawPayload);
+
+        $this->assertStringContainsString('Hello world', $result);
+    }
+}
