5b94ff421a81476e0093e0fdc09e3f62a49cdb97
emilschn <emilien.schneider@beta.gouv.fr>
2026-03-04 16:27:49 +0100

    add constraints to address fields  #5296

--- src/Form/SignalementeEditFO/AdresseLogementType.php
+++ src/Form/SignalementeEditFO/AdresseLogementType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class AdresseLogementType extends AbstractType
 {
@@ -19,18 +20,46 @@ class AdresseLogementType extends AbstractType
         $builder
             ->add('etageOccupant', TextType::class, [
                 'label' => 'Étage',
+                'help' => 'Format attendu : 5 caractères maximum',
+                'constraints' => [
+                    new Assert\Length([
+                        'max' => 5,
+                        'maxMessage' => 'L\'étage doit contenir au maximum {{ limit }} caractères.',
+                    ]),
+                ],
                 'required' => false,
             ])
             ->add('escalierOccupant', TextType::class, [
                 'label' => 'Escalier',
+                'help' => 'Format attendu : 3 caractères maximum',
+                'constraints' => [
+                    new Assert\Length([
+                        'max' => 3,
+                        'maxMessage' => 'L\'escalier doit contenir au maximum {{ limit }} caractères.',
+                    ]),
+                ],
                 'required' => false,
             ])
             ->add('numAppartOccupant', TextType::class, [
                 'label' => 'Numéro d\'appartement',
+                'help' => 'Format attendu : 5 caractères maximum',
+                'constraints' => [
+                    new Assert\Length([
+                        'max' => 5,
+                        'maxMessage' => 'Le numéro d\'appartement doit contenir au maximum {{ limit }} caractères.',
+                    ]),
+                ],
                 'required' => false,
             ])
             ->add('adresseAutreOccupant', TextType::class, [
                 'label' => 'Autre',
+                'help' => 'Format attendu : 255 caractères maximum',
+                'constraints' => [
+                    new Assert\Length([
+                        'max' => 255,
+                        'maxMessage' => 'Les autres informations sur l\'adresse doivent contenir au maximum {{ limit }} caractères.',
+                    ]),
+                ],
                 'required' => false,
             ]);
 
