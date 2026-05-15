2b6fc8917ee513d16b4a474c74b5bd864c0f7817
numew <numabord@gmail.com>
2026-05-15 14:00:37 +0200

    adapt #5873

--- src/Entity/Enum/SuiviCategory.php
+++ src/Entity/Enum/SuiviCategory.php
@@ -231,7 +231,6 @@ enum SuiviCategory: string
             case self::INJONCTION_BAILLEUR_BASCULE_PROCEDURE_PAR_BAILLEUR_COMMENTAIRE:
             case self::INJONCTION_BAILLEUR_DEMANDE_CLOTURE_PAR_BAILLEUR:
             case self::INJONCTION_BAILLEUR_DEMANDE_CLOTURE_PAR_BAILLEUR_COMMENTAIRE:
-            case self::MESSAGE_BAILLEUR:
             case self::ASK_DOCUMENT:
                 return Suivi::TYPE_AUTO;
             case self::ASK_FEEDBACK_SENT:
@@ -245,6 +244,7 @@ enum SuiviCategory: string
             case self::DEMANDE_ABANDON_PROCEDURE:
             case self::DEMANDE_POURSUITE_PROCEDURE:
             case self::INJONCTION_BAILLEUR_BASCULE_PROCEDURE_PAR_USAGER:
+            case self::MESSAGE_BAILLEUR:
                 return Suivi::TYPE_USAGER;
             case self::MESSAGE_USAGER_POST_CLOTURE:
                 return Suivi::TYPE_USAGER_POST_CLOTURE;
