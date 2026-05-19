82c0beb417b8a2598c3373c67d72c678ad7599ce
Hélène Meneuvrier <33071753+hmeneuvrier@users.noreply.github.com>
2026-05-19 12:28:50 +0200

    improve history affectations #5851 (#5875)

--- src/Repository/HistoryEntryRepository.php
+++ src/Repository/HistoryEntryRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Enum\HistoryEntryEvent;
 use App\Entity\HistoryEntry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -19,5 +20,22 @@ class HistoryEntryRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, HistoryEntry::class);
+    }
+
+    public function findFirstEntityUpdateAfter(int $entityId, string $entityName, \DateTimeImmutable $afterDate): ?HistoryEntry
+    {
+        return $this->createQueryBuilder('h')
+            ->where('h.entityId = :entityId')
+            ->andWhere('h.entityName = :entityName')
+            ->andWhere('h.event = :event')
+            ->andWhere('h.createdAt > :date')
+            ->setParameter('entityId', $entityId)
+            ->setParameter('entityName', $entityName)
+            ->setParameter('event', HistoryEntryEvent::UPDATE)
+            ->setParameter('date', $afterDate)
+            ->orderBy('h.createdAt', 'ASC')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 }
