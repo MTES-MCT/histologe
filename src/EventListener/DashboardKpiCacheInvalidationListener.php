35e1b18eea125e1c4fbf835853f5bf931b3035dc
Saidi <saidi.ahamada@gmail.com>
2026-06-03 19:58:07 +0200

    add new case to invalidate #5872

--- src/EventListener/DashboardKpiCacheInvalidationListener.php
+++ src/EventListener/DashboardKpiCacheInvalidationListener.php
@@ -7,6 +7,7 @@ use App\Entity\Enum\AffectationStatus;
 use App\Entity\Enum\SignalementStatus;
 use App\Entity\Enum\SuiviCategory;
 use App\Entity\FailedEmail;
+use App\Entity\Notification;
 use App\Entity\Signalement;
 use App\Entity\Suivi;
 use App\Entity\UserSignalementSubscription;
@@ -76,6 +77,14 @@ final readonly class DashboardKpiCacheInvalidationListener
             if (in_array($entity->getCategory(), SuiviCategory::getSuiviTypeActiviteUsager(), true)) {
                 // DOSSIERS_MESSAGES_PARTNERS - Nouveaux messages
                 // DOSSIERS_MESSAGES_PARTNERS - Messages après fermeture
+                // DOSSIERS_MESSAGES_PARTNERS - Messages usagers n'ayant pas eu de réponse
+                $this->invalidateTerritoryKpiTags(
+                    TabCountKpiCacheHelper::DOSSIERS_MESSAGES_USAGERS,
+                    $territoryId
+                );
+            }
+
+            if ($entity->getIsVisibleForUsager() && SuiviCategory::MESSAGE_PARTNER == $entity->getCategory()) {
                 // DOSSIERS_MESSAGES_PARTNERS - Messages usagers n'ayant pas eu de réponse
                 $this->invalidateTerritoryKpiTags(
                     TabCountKpiCacheHelper::DOSSIERS_MESSAGES_USAGERS,
@@ -169,6 +178,20 @@ final readonly class DashboardKpiCacheInvalidationListener
                     $territoryId
                 );
             }
+        }
+
+        if ($entity instanceof Notification) {
+            $territoryId = $entity->getSignalement()?->getTerritory()?->getId();
+            if (null === $territoryId) {
+                return;
+            }
+
+            // DOSSIERS_MESSAGES_PARTNERS - Nouveaux messages
+            // DOSSIERS_MESSAGES_PARTNERS - Messages après fermeture
+            $this->invalidateTerritoryKpiTags(
+                TabCountKpiCacheHelper::DOSSIERS_MESSAGES_USAGERS,
+                $territoryId
+            );
         }
     }
 
