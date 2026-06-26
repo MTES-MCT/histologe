b58b6faff0514a8460c7dcaf31ea886b39a8e222
numew <numabord@gmail.com>
2026-06-26 17:53:19 +0200

    wip fix tests #6032

--- src/Service/History/HistoryEntryBuffer.php
+++ src/Service/History/HistoryEntryBuffer.php
@@ -42,7 +42,7 @@ class HistoryEntryBuffer
 
     public function flushPendingHistoryEntries(): void
     {
-        if (empty($this->pendingHistoryEntries) || !$this->entityManager->isOpen()) {
+        if (empty($this->pendingHistoryEntries)/* || !$this->entityManager->isOpen() */) {
             return;
         }
         // for prevent flushing invalid entities
