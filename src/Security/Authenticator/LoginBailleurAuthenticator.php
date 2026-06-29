023fdf15a6b267f35a159e00707ea80519e92fc7
emilschn <emilien.schneider@beta.gouv.fr>
2026-06-29 18:28:02 +0200

    add csrf control #6068

--- src/Security/Authenticator/LoginBailleurAuthenticator.php
+++ src/Security/Authenticator/LoginBailleurAuthenticator.php
@@ -12,6 +12,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
@@ -34,6 +35,7 @@ class LoginBailleurAuthenticator extends AbstractLoginFormAuthenticator
     {
         $reference = (string) $request->request->get('bailleur_reference');
         $code = (string) $request->request->get('bailleur_code');
+        $csrfToken = (string) $request->request->get('_csrf_token');
 
         if (empty($reference)) {
             throw new CustomUserMessageAuthenticationException('Veuillez saisir la référence du dossier.');
@@ -49,7 +51,12 @@ class LoginBailleurAuthenticator extends AbstractLoginFormAuthenticator
             throw new CustomUserMessageAuthenticationException('La référence et/ou le code ne sont pas valides.');
         }
 
-        return new SelfValidatingPassport(new UserBadge($signalement->getUuid()));
+        return new SelfValidatingPassport(
+            new UserBadge($signalement->getUuid()),
+            [
+                new CsrfTokenBadge('authenticate_bailleur', $csrfToken),
+            ]
+        );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
