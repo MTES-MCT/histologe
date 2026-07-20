01aa3e7c25cfe12c6cbc836cf878a4d6634dc6e8
emilschn <emilien.schneider@beta.gouv.fr>
2026-07-20 15:19:06 +0200

    add address entry sync with signalements #5949

--- tests/Functional/Manager/SignalementManagerTest.php
+++ tests/Functional/Manager/SignalementManagerTest.php
@@ -19,6 +19,7 @@ use App\Factory\SignalementAffectationListViewFactory;
 use App\Factory\SignalementExportFactory;
 use App\Factory\SignalementImportFactory;
 use App\Factory\SuiviDelayedFactory;
+use App\Manager\AddressManager;
 use App\Manager\AffectationManager;
 use App\Manager\SignalementManager;
 use App\Manager\SuiviManager;
@@ -63,6 +64,7 @@ class SignalementManagerTest extends WebTestCase
     private SuiviManager $suiviManager;
     private SuiviDelayedFactory $suiviDelayedFactory;
     private UserManager $userManager;
+    private AddressManager $addressManager;
     private BailleurRepository $bailleurRepository;
     private PartnerRepository $partnerRepository;
     private SignalementRepository $signalementRepository;
@@ -95,6 +97,7 @@ class SignalementManagerTest extends WebTestCase
         $this->suiviManager = static::getContainer()->get(SuiviManager::class);
         $this->suiviDelayedFactory = static::getContainer()->get(SuiviDelayedFactory::class);
         $this->userManager = static::getContainer()->get(UserManager::class);
+        $this->addressManager = static::getContainer()->get(AddressManager::class);
         $this->bailleurRepository = static::getContainer()->get(BailleurRepository::class);
         $this->partnerRepository = static::getContainer()->get(PartnerRepository::class);
         $this->signalementRepository = static::getContainer()->get(SignalementRepository::class);
@@ -120,6 +123,7 @@ class SignalementManagerTest extends WebTestCase
             $this->suiviManager,
             $this->suiviDelayedFactory,
             $this->userManager,
+            $this->addressManager,
             $this->bailleurRepository,
             $this->partnerRepository,
             $this->signalementRepository,
