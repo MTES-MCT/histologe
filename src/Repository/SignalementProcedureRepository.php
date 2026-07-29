36d98c31e60f1700880710733ced5e9b9f105d29
Numew <numabord@gmail.com>
2026-07-29 14:19:56 +0200

    remove auto-generated code #6044

--- src/Repository/SignalementProcedureRepository.php
+++ src/Repository/SignalementProcedureRepository.php
@@ -15,29 +15,4 @@ class SignalementProcedureRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, SignalementProcedure::class);
     }
-
-    //    /**
-    //     * @return SignalementProcedure[] Returns an array of SignalementProcedure objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('s.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?SignalementProcedure
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
