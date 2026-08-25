b09298fa35b2347d48643e6ab7ac3aba770e10c1
hmeneuvrier <hmeneuvrier@gmail.com>
2026-08-25 08:31:01 +0200

    add anomaly #6204

--- src/Entity/Enum/SignalementAddressAnomaly.php
+++ src/Entity/Enum/SignalementAddressAnomaly.php
@@ -10,6 +10,8 @@ enum SignalementAddressAnomaly: string
 
     case MISSING_CP_AND_INSEE = 'MISSING_CP_AND_INSEE';
     case INVALID_INSEE_FORMAT = 'INVALID_INSEE_FORMAT';
+    case INVALID_CP_FORMAT = 'INVALID_CP_FORMAT';
+    case INCONSISTENT_CP_INSEE = 'INCONSISTENT_CP_INSEE';
     case INCONSISTENT_TERRITORY = 'INCONSISTENT_TERRITORY';
 
     /** @return array<string, string> */
@@ -18,6 +20,8 @@ enum SignalementAddressAnomaly: string
         return [
             self::MISSING_CP_AND_INSEE->name => 'Absence de code postal et de code INSEE',
             self::INVALID_INSEE_FORMAT->name => 'Format du code INSEE invalide',
+            self::INVALID_CP_FORMAT->name => 'Format du code postal invalide',
+            self::INCONSISTENT_CP_INSEE->name => 'Code postal et code INSEE incohérents entre eux',
             self::INCONSISTENT_TERRITORY->name => 'Territoire incohérent',
         ];
     }
--- src/Service/Signalement/SignalementAddressAnomalyChecker.php
+++ src/Service/Signalement/SignalementAddressAnomalyChecker.php
@@ -5,11 +5,16 @@ namespace App\Service\Signalement;
 use App\Entity\Enum\SignalementAddressAnomaly;
 use App\Entity\Signalement;
 use App\Entity\Territory;
+use App\Repository\CommuneRepository;
 
 class SignalementAddressAnomalyChecker
 {
+    private const string INSEE_FORMAT_PATTERN = '/^(\d{2}|2[AB])\d{3}$/';
+    private const string CP_FORMAT_PATTERN = '/^\d{5}$/';
+
     public function __construct(
         private readonly ZipcodeProvider $zipcodeProvider,
+        private readonly CommuneRepository $communeRepository,
     ) {
     }
 
@@ -38,8 +43,24 @@ class SignalementAddressAnomalyChecker
 
         $errors = [];
 
-        if ($inseeOccupant && 5 !== strlen($inseeOccupant)) {
+        // le code INSEE fait 5 caractères mais n'est pas forcément 5 chiffres : la Corse utilise
+        // "2A"/"2B" comme préfixe de département (ex: 2A004), les DROM restent numériques.
+        $inseeValid = !$inseeOccupant || 1 === preg_match(self::INSEE_FORMAT_PATTERN, $inseeOccupant);
+        if (!$inseeValid) {
             $errors[] = SignalementAddressAnomaly::INVALID_INSEE_FORMAT;
+        }
+
+        $cpValid = !$cpOccupant || 1 === preg_match(self::CP_FORMAT_PATTERN, $cpOccupant);
+        if (!$cpValid) {
+            $errors[] = SignalementAddressAnomaly::INVALID_CP_FORMAT;
+        }
+
+        // ne vérifie la cohérence de la paire que si les deux formats sont individuellement valides,
+        // pour éviter une erreur redondante avec INVALID_INSEE_FORMAT / INVALID_CP_FORMAT.
+        if ($inseeOccupant && $cpOccupant && $inseeValid && $cpValid
+            && null === $this->communeRepository->findOneBy(['codePostal' => $cpOccupant, 'codeInsee' => $inseeOccupant])
+        ) {
+            $errors[] = SignalementAddressAnomaly::INCONSISTENT_CP_INSEE;
         }
 
         $calculatedTerritory = $this->getCalculatedTerritory($signalement);
