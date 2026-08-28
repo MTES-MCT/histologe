d0cba9f33e1570f7f258b25b08cac0d668665fb6
emilschn <emilien.schneider@beta.gouv.fr>
2026-08-28 12:13:54 +0200

    wip panels for edit visites #6229

--- assets/scripts/vanilla/controllers/form_visite.js
+++ assets/scripts/vanilla/controllers/form_visite.js
@@ -222,7 +222,6 @@ if (modalAddVisite) {
       if (visiteForm) {
         const matchAdd = visiteForm.id.match(/form-add-visite/);
         const matchEdit = visiteForm.className.match(/form-(edit|reschedule|confirm)-visite/);
-
         if (!matchAdd && !matchEdit) return;
 
         let idIntervention = null;
