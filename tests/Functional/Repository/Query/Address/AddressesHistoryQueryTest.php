948ee9ad1ffe17cd807f76e72d192f131067429f
emilschn <emilien.schneider@beta.gouv.fr>
2026-09-01 17:10:28 +0200

    fix tests #5949

--- tests/Functional/Repository/Query/Address/AddressesHistoryQueryTest.php
+++ tests/Functional/Repository/Query/Address/AddressesHistoryQueryTest.php
@@ -10,6 +10,7 @@ use App\Entity\Territory;
 use App\Entity\User;
 use App\Entity\Zone;
 use App\Repository\Query\Address\AddressesHistoryQuery;
+use App\Repository\TerritoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -23,6 +24,47 @@ class AddressesHistoryQueryTest extends KernelTestCase
         self::bootKernel();
         $this->entityManager = static::getContainer()->get(EntityManagerInterface::class);
         $this->addressesHistoryQuery = new AddressesHistoryQuery($this->entityManager);
+    }
+
+    public function testFindAllList(): void
+    {
+        /** @var AddressesHistoryQuery $addressesHistoryQuery */
+        $addressesHistoryQuery = new AddressesHistoryQuery($this->entityManager);
+
+        // Test sans territoire - devrait retourner toutes les adresses
+        $allAddresses = $addressesHistoryQuery->findAllList();
+
+        $this->assertIsArray($allAddresses);
+        $this->assertNotEmpty($allAddresses);
+        $this->assertCount(60, $allAddresses);
+
+        // Vérifie la structure des résultats
+        foreach ($allAddresses as $address) {
+            $this->assertArrayHasKey('id', $address);
+            $this->assertArrayHasKey('address', $address);
+            $this->assertIsString($address['address']);
+            $this->assertNotEmpty($address['address']);
+        }
+
+        // Vérifie qu'il n'y a pas de doublons d'IDs
+        $ids = array_column($allAddresses, 'id');
+        $this->assertEquals(count($ids), count(array_unique($ids)));
+    }
+
+    public function testFindAllListWithTerritory(): void
+    {
+        /** @var AddressesHistoryQuery $addressesHistoryQuery */
+        $addressesHistoryQuery = new AddressesHistoryQuery($this->entityManager);
+        /** @var TerritoryRepository $territoryRepository */
+        $territoryRepository = $this->entityManager->getRepository(Territory::class);
+
+        $territory = $territoryRepository->findOneBy(['zip' => '13']);
+        $this->assertNotNull($territory, 'Territory with zip 13 should exist in fixtures');
+
+        $addressesForTerritory = $addressesHistoryQuery->findAllList($territory);
+
+        $this->assertIsArray($addressesForTerritory);
+        $this->assertCount(21, $addressesForTerritory);
     }
 
     public function testFindAddressesWithHistoryForSuperAdmin(): void
@@ -265,7 +307,7 @@ class AddressesHistoryQueryTest extends KernelTestCase
         $results = $this->addressesHistoryQuery->findAddressesWithHistory($user, $searchQuery);
 
         $this->assertIsArray($results);
-        $this->assertCount(0, $results); // TODO : ajuster quand fixtures adresses gérées dans la PR #6212
+        $this->assertCount(2, $results);
 
         // Test avec plusieurs bailleurs
         $searchQuery = new AddressesHistorySearchQuery(
@@ -275,7 +317,7 @@ class AddressesHistoryQueryTest extends KernelTestCase
         $results = $this->addressesHistoryQuery->findAddressesWithHistory($user, $searchQuery);
 
         $this->assertIsArray($results);
-        $this->assertCount(4, $results); // TODO : ajuster quand fixtures adresses gérées dans la PR #6212
+        $this->assertCount(6, $results);
     }
 
     public function testFindAddressesWithHistoryWithTypesArretesFilter(): void
@@ -320,7 +362,7 @@ class AddressesHistoryQueryTest extends KernelTestCase
         $results = $this->addressesHistoryQuery->findAddressesWithHistory($user, $searchQuery);
 
         $this->assertIsArray($results);
-        $this->assertCount(5, $results); // TODO : ajuster quand fixtures adresses gérées dans la PR #6212
+        $this->assertCount(29, $results);
 
         // Test avec un mélange de commune et EPCI (préfixé par "EPCI : ")
         $searchQuery = new AddressesHistorySearchQuery(
@@ -330,7 +372,7 @@ class AddressesHistoryQueryTest extends KernelTestCase
         $results = $this->addressesHistoryQuery->findAddressesWithHistory($user, $searchQuery);
 
         $this->assertIsArray($results);
-        $this->assertCount(5, $results); // TODO : ajuster quand fixtures adresses gérées dans la PR #6212
+        $this->assertCount(34, $results);
     }
 
     public function testFindAddressesWithHistoryWithZoneFilter(): void
@@ -349,7 +391,7 @@ class AddressesHistoryQueryTest extends KernelTestCase
             $results = $this->addressesHistoryQuery->findAddressesWithHistory($user, $searchQuery);
 
             $this->assertIsArray($results);
-            $this->assertCount(2, $results);
+            $this->assertCount(5, $results);
         }
     }
 }
