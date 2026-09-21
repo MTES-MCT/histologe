27affcbb81ae76f356c969d32d34dfa980fed174
Numew <numabord@gmail.com>
2026-09-21 16:47:06 +0200

    fix tests #6329

--- tests/Functional/Controller/SignalementEditControllerTest.php
+++ tests/Functional/Controller/SignalementEditControllerTest.php
@@ -390,7 +390,8 @@ class SignalementEditControllerTest extends WebTestCase
             'usager_situation_foyer[isLogementSocial]' => '1',
             'usager_situation_foyer[isRelogement]' => '1',
             'usager_situation_foyer[numAllocataire]' => '11223344',
-            'usager_situation_foyer[allocataire]' => 'non',
+            'usager_situation_foyer[allocataire]' => 'oui',
+            'usager_situation_foyer[caisseAllocation]' => 'MSA',
             'usager_situation_foyer[montantAllocation]' => '555',
         ]);
 
